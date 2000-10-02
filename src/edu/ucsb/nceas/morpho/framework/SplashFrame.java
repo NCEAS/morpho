008fae5b2a568bcb16b8f9b8764565560a1ad43d
Dan Higgins <higgins@nceas.ucsb.edu>
2000-10-02 22:57:51 +0000

    Added "Loading - Please Wait" message on first use

--- src/edu/ucsb/nceas/morpho/framework/SplashFrame.java
+++ src/edu/ucsb/nceas/morpho/framework/SplashFrame.java
@@ -26,28 +26,33 @@ public class SplashFrame extends javax.swing.JFrame
 		JPanel3.setBounds(0,0,427,330);
 		JPanel1.setLayout(new BorderLayout(0,0));
 		JPanel3.add(BorderLayout.NORTH,JPanel1);
-		JPanel1.setBounds(0,0,427,60);
+		JPanel1.setBounds(0,0,427,31);
+		LoadingLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+		LoadingLabel.setText("Now Loading!!! ... Please Wait");
+		JPanel1.add(BorderLayout.NORTH,LoadingLabel);
+		LoadingLabel.setBounds(0,0,427,15);
+		LoadingLabel.setVisible(false);
 		JLabel3.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 		JLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
 		JLabel3.setText("MORPHO");
 		JPanel1.add(BorderLayout.CENTER,JLabel3);
 		JLabel3.setForeground(java.awt.Color.blue);
 		JLabel3.setFont(new Font("Dialog", Font.BOLD|Font.ITALIC, 36));
-		JLabel3.setBounds(0,0,427,44);
+		JLabel3.setBounds(0,0,427,42);
 		JLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
 		JLabel1.setText("Data Managment for Ecologists");
 		JPanel1.add(BorderLayout.SOUTH,JLabel1);
 		JLabel1.setForeground(java.awt.Color.black);
 		JLabel1.setFont(new Font("Dialog", Font.BOLD, 14));
-		JLabel1.setBounds(0,44,427,16);
+		JLabel1.setBounds(0,15,427,16);
 		JScrollPane1.setOpaque(true);
 		JPanel3.add(BorderLayout.CENTER,JScrollPane1);
-		JScrollPane1.setBounds(0,60,427,255);
+		JScrollPane1.setBounds(0,31,427,284);
 		JTextArea1.setEditable(false);
 		JTextArea1.setWrapStyleWord(true);
 		JTextArea1.setLineWrap(true);
 		JScrollPane1.getViewport().add(JTextArea1);
-		JTextArea1.setBounds(0,0,424,252);
+		JTextArea1.setBounds(0,0,409,300);
 		String temp = "This material is based upon work supported by ";
 		temp = temp + "the National Science Foundation under Grant No. DEB99-80154 and ";
 		temp = temp + "Grant No. DBI99-04777. Also supported by the National Center for Ecological Analysis ";
@@ -67,7 +72,7 @@ public class SplashFrame extends javax.swing.JFrame
 		temp = temp + "viewing, with the most common color being blue.";
 		JTextArea1.setText("This material is based upon work supported by the National Science Foundation under Grant No. DEB99-80154 and Grant No. DBI99-04777. Also supported by the National Center for Ecological Analysis and Synthesis, a Center funded by NSF (Grant #DEB-94-21535),the University of California - Santa Barbara, and the State of California. Any opinions, findings and conclusions or recommendations expressed in this material are those of the author(s) and do not necessarily reflect the views of the National Science Foundation (NSF).\n\n Participating organizations include the National Center for Ecological Analysis and Synthesis (NCEAS), the Long Term Ecological Research Network (LTER), Texas Tech University, and the San Diego Supercomputer Center\n\n Participating individuals include Matt Jones, Dan Higgins, Jivka Bojilova, Chad Berkley, Ruldolf Nottrott, and others\n\nThis software is named after the Morpho butterfly which is common in South America. The apparent color of this butterfly is highly dependent on the angle of viewing, with the most common color being blue.");
 		JLabel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		JLabel8.setText("Version 0.75 - Sept 2000");
+		JLabel8.setText("Version 0.80 - Oct 2000");
 		JPanel3.add(BorderLayout.SOUTH,JLabel8);
 		JLabel8.setForeground(java.awt.Color.black);
 		JLabel8.setFont(new Font("Dialog", Font.PLAIN, 12));
@@ -101,6 +106,17 @@ public class SplashFrame extends javax.swing.JFrame
 	{
 		this();
 		setTitle(sTitle);
+	}
+	public SplashFrame(boolean showLoading)
+	{
+		this();
+		if(showLoading) {LoadingLabel.setVisible(showLoading);}
+	}
+	public SplashFrame(String sTitle, boolean showLoading)
+	{
+		this();
+		setTitle(sTitle);
+		if(showLoading) {LoadingLabel.setVisible(showLoading);}
 	}
 
 	public void setVisible(boolean b)
@@ -141,6 +157,7 @@ public class SplashFrame extends javax.swing.JFrame
 	//{{DECLARE_CONTROLS
 	javax.swing.JPanel JPanel3 = new javax.swing.JPanel();
 	javax.swing.JPanel JPanel1 = new javax.swing.JPanel();
+	javax.swing.JLabel LoadingLabel = new javax.swing.JLabel();
 	javax.swing.JLabel JLabel3 = new javax.swing.JLabel();
 	javax.swing.JLabel JLabel1 = new javax.swing.JLabel();
 	javax.swing.JScrollPane JScrollPane1 = new javax.swing.JScrollPane();
