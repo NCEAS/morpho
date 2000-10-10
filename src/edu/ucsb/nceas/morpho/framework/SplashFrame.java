876e5aa2bb5998a138ee93d28aa9544cc0886fab
Dan Higgins <higgins@nceas.ucsb.edu>
2000-10-10 18:21:34 +0000

    update to current version

--- src/edu/ucsb/nceas/morpho/framework/SplashFrame.java
+++ src/edu/ucsb/nceas/morpho/framework/SplashFrame.java
@@ -70,7 +70,7 @@ public class SplashFrame extends javax.swing.JFrame
         temp = temp + "Texas Tech University, and the San Diego Supercomputer Center";
 		temp = temp + "\n\n Participating individuals include Matt Jones, Dan Higgins, Jivka Bojilova, Chad Berkley, ";
 		temp = temp + "Ruldolf Nottrott, and others";
-		temp = temp + "\n\nThis software is named after the Morpho butterfly which is common in ";
+		temp = temp + "\n\nThis software is named after butterflies of the genus MORPHO which are common in ";
 		temp = temp + "South America. The apparent color of this butterfly is highly dependent on the angle of ";
 		temp = temp + "viewing, with the most common color being blue.";
 		JTextArea1.setText("This material is based upon work supported by the National Science Foundation under Grant No. DEB99-80154 and Grant No. DBI99-04777. Also supported by the National Center for Ecological Analysis and Synthesis, a Center funded by NSF (Grant #DEB-94-21535),the University of California - Santa Barbara, and the State of California. Any opinions, findings and conclusions or recommendations expressed in this material are those of the author(s) and do not necessarily reflect the views of the National Science Foundation (NSF).\n\n Participating organizations include the National Center for Ecological Analysis and Synthesis (NCEAS), the Long Term Ecological Research Network (LTER), Texas Tech University, and the San Diego Supercomputer Center\n\n Participating individuals include Matt Jones, Dan Higgins, Jivka Bojilova, Chad Berkley, Ruldolf Nottrott, and others\n\nThis software is named after the Morpho butterfly which is common in South America. The apparent color of this butterfly is highly dependent on the angle of viewing, with the most common color being blue.");
