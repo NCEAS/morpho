efc1736b40e1ba4daf55b3b4f4375394ab9325e4
Matt Jones <jones@nceas.ucsb.edu>
2001-04-17 01:07:47 +0000

    Reformat source code to be 80 columns wide max for text editing, added license terms.

--- src/edu/ucsb/nceas/morpho/framework/PluginInterface.java
+++ src/edu/ucsb/nceas/morpho/framework/PluginInterface.java
@@ -1,17 +1,35 @@
 /**
- *        Name: PluginInterface.java
- *     Purpose: An interface representing the methods that all plugin
- *		components must implement
- *   Copyright: 2000 Regents of the University of California and the
- *              National Center for Ecological Analysis and Synthesis
- *     Authors: Matt Jones
+ *       Name: PluginInterface.java
+ *    Purpose: An interface representing the methods that all plugin
+ *             components must implement
+ *  Copyright: 2000 Regents of the University of California and the
+ *             National Center for Ecological Analysis and Synthesis
+ *    Authors: Matt Jones
+ *    Release: @release@
  *
- *     Version: '$Id: PluginInterface.java,v 1.2 2000-11-30 19:45:04 higgins Exp $'
+ *   '$Author: jones $'
+ *     '$Date: 2001-04-17 01:07:47 $'
+ * '$Revision: 1.2.2.1 $'
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+
 package edu.ucsb.nceas.dtclient;
 
 import javax.swing.Action;
- 
+
 /**
  * All component plugins that are to be included in the ClientFramework
  * must implement this interface so that the services of the ClientFramework
@@ -40,13 +58,12 @@ public interface PluginInterface
   /**
    * The plugin must return the list of Actions to be associated with the
    * toolbar for the framework. 
-   */ 
+   */
   public Action[] registerToolbarActions();
-  
-  
+
+
   /** 
-  * The plugin must be able to get an instance of ClientFramework
-  * added by DFH, Nov 2000
-  */
+   * The plugin must be able to get an instance of ClientFramework
+   */
   public void setContainer(Object o);
 }
