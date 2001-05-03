9dd63ab5570366da33121660b5f794be4075d385
Chad Berkley <berkley@nceas.ucsb.edu>
2001-05-03 18:56:12 +0000

    class to handle multiple triples

---
+++ src/edu/ucsb/nceas/morpho/datapackage/TripleCollection.java
@@ -0,0 +1,149 @@
+/**
+ *  '$RCSfile: TripleCollection.java,v $'
+ *  Copyright: 2000 Regents of the University of California and the
+ *              National Center for Ecological Analysis and Synthesis
+ *    Authors: @authors@
+ *    Release: @release@
+ *
+ *   '$Author: berkley $'
+ *     '$Date: 2001-05-03 18:56:12 $'
+ * '$Revision: 1.1 $'
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.ucsb.nceas.morpho.datapackage;
+
+import java.util.*;
+
+/**
+ * This class implements a collection of triples which make up all of the
+ * relationships in a package.
+ */
+public class TripleCollection
+{
+  private Vector triples = new Vector();
+  /**
+   * Default Constructor
+   */
+  public TripleCollection()
+  {
+    
+  }
+  
+  /**
+   * create a collection of triples from a vector of Triple objects.
+   */
+  public TripleCollection(Vector triples)
+  {
+    this.triples = new Vector(triples);
+  }
+  
+  /**
+   * Add a single triple to this collection
+   */
+  public void addTriple(Triple triple)
+  {
+    this.triples.addElement(triple);
+  }
+  
+  /**
+   * removes the specified triple from the collection and returns it.  If the
+   * triple was not in the collection null is returned.
+   */ 
+  public Triple removeTriple(Triple triple)
+  {
+    boolean removed = triples.remove(triple);
+    if(removed)
+    {
+      return triple;
+    }
+    else
+    {
+      return null;
+    }
+  }
+  
+  /**
+   * returns a vector of triples with the given subject
+   */
+  public Vector getCollectionBySubject(String subject)
+  {
+    Vector trips = new Vector();
+    for(int i=0; i<triples.size(); i++)
+    {
+      Triple trip = new Triple((Triple)triples.elementAt(i));
+      if(trip.getSubject().equals(subject))
+      {
+        trips.addElement(trip);
+      }
+    }
+    return trips;
+  }
+  
+  /**
+   * returns a vector of triples with the given relationship
+   */
+  public Vector getCollectionByRelationship(String relationship)
+  {
+    Vector trips = new Vector();
+    for(int i=0; i<triples.size(); i++)
+    {
+      Triple trip = new Triple((Triple)triples.elementAt(i));
+      if(trip.getSubject().equals(relationship))
+      {
+        trips.addElement(trip);
+      }
+    }
+    return trips;
+  }
+  
+  /**
+   * returns a vector of triples with the given object
+   */
+  public Vector getCollectionByObject(String object)
+  {
+    Vector trips = new Vector();
+    for(int i=0; i<triples.size(); i++)
+    {
+      Triple trip = new Triple((Triple)triples.elementAt(i));
+      if(trip.getSubject().equals(object))
+      {
+        trips.addElement(trip);
+      }
+    }
+    return trips;
+  }
+  
+  /**
+   * return this collection as a string
+   */
+  public String toString()
+  {
+    StringBuffer sb = new StringBuffer();
+    sb.append("[");
+    for(int i=0; i<triples.size(); i++)
+    {
+      Triple t = new Triple((Triple)triples.elementAt(i));
+      sb.append(t.toString());
+      if(i != triples.size()-1)
+      {
+        sb.append(",");
+      }
+    }
+    sb.append("]");
+    return sb.toString();
+  }
+}
