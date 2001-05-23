0a8ee1d0a0a3f026001235f37b4eb9926bc32915
Dan Higgins <higgins@nceas.ucsb.edu>
2001-05-23 16:07:26 +0000

    fixed bug in classpath reference to icon jar

--- morpho.bat
+++ morpho.bat
@@ -1,1 +1,1 @@
-java -cp .;lib/xerces.jar;lib/xalan.jar;lib/xml4j.jar;lib/sym.jar;lib/morpho.jar;lib/jlf-1_0.jar edu.ucsb.nceas.morpho.framework.ClientFramework
+java -cp .;lib/xerces.jar;lib/xalan.jar;lib/xml4j.jar;lib/sym.jar;lib/morpho.jar;lib/jlfgr-1_0.jar edu.ucsb.nceas.morpho.framework.ClientFramework
