2e12a7231a1045c6d42753dbf518bb04583d0d98
Matt Jones <jones@nceas.ucsb.edu>
2001-06-10 02:36:31 +0000

    Refined the dialog UI layout.  Began work on creating the profile.

--- src/edu/ucsb/nceas/morpho/framework/ProfileDialog.java
+++ src/edu/ucsb/nceas/morpho/framework/ProfileDialog.java
@@ -6,8 +6,8 @@
  *    Release: @release@
  *
  *   '$Author: jones $'
- *     '$Date: 2001-06-07 01:33:33 $'
- * '$Revision: 1.1 $'
+ *     '$Date: 2001-06-10 02:36:14 $'
+ * '$Revision: 1.2 $'
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -41,17 +41,24 @@ public class ProfileDialog extends JDialog
 {
   ConfigXML config;
   ClientFramework container = null;
+  /** the total number of screens to be processed */
+  int numScreens;
+  /** the screen currently displaying (indexed from 0 to numScreens-1) */
+  int currentScreen;
 
   JLabel helpLabel = new JLabel();
   JPanel screenPanel = null;
+  JButton previousButton = null;
+  JButton nextButton = null;
+  JButton cancelButton = new JButton();
+
+  ImageIcon forwardIcon = null;
   JTextField firstNameField = new JTextField();
   JTextField lastNameField = new JTextField();
   JTextField usernameField = new JTextField();
   JPasswordField passwordField = new JPasswordField();
   JPasswordField passwordField2 = new JPasswordField();
-  JButton previousButton = null;
-  JButton nextButton = null;
-  JButton cancelButton = new JButton();
+  JTextField constructionField = new JTextField();
 
   /**
    * Construct a dialog and set the framework
@@ -71,31 +78,33 @@ public class ProfileDialog extends JDialog
 
     container = cont;
 
+    numScreens = 2;
+    currentScreen = 0;
+
     setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     setTitle("New Profile");
-    setSize(650,400);
     getContentPane().setLayout(new BoxLayout(getContentPane(),
                                BoxLayout.X_AXIS));
-    // Center the Frame
-    Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
-    Rectangle frameDim = getBounds();
-    setLocation((screenDim.width - frameDim.width) / 2 ,
-            (screenDim.height - frameDim.height) /2);
     setVisible(false);
 
     JPanel helpPanel = new JPanel();
     helpPanel.setLayout(new BoxLayout(helpPanel, BoxLayout.Y_AXIS));
     helpPanel.setBackground(Color.white);
-    helpPanel.setMinimumSize(new Dimension(100,400));
-    helpPanel.setPreferredSize(new Dimension(100,400));
+    helpPanel.setMaximumSize(new Dimension(150,400));
+    helpPanel.setPreferredSize(new Dimension(150,400));
     helpPanel.setBorder(BorderFactory.createLoweredBevelBorder());
+    helpPanel.add(Box.createRigidArea(new Dimension(8,8)));
+    ImageIcon logoIcon = 
+              new ImageIcon(getClass().getResource("logo-icon.gif"));
+    JLabel imageLabel = new JLabel();
+    imageLabel.setIcon(logoIcon);
+    helpPanel.add(imageLabel);
     helpLabel.setText(
               "<html>This is a whole bunch of help text.</html>");
     helpLabel.setForeground(Color.black);
     helpLabel.setFont(new Font("Dialog", Font.BOLD, 12));
     helpLabel.setBorder(BorderFactory.createEmptyBorder(8,8,8,8));
     helpLabel.setVerticalAlignment(SwingConstants.TOP);
-    helpLabel.setMinimumSize(new Dimension(100,400));
     helpPanel.add(helpLabel);
     getContentPane().add(helpPanel);
 
@@ -103,7 +112,6 @@ public class ProfileDialog extends JDialog
 
     JPanel entryPanel = new JPanel();
     entryPanel.setLayout(new BoxLayout(entryPanel, BoxLayout.Y_AXIS));
-    entryPanel.setBackground(Color.red);
     entryPanel.add(Box.createVerticalStrut(8));
 
     JLabel headLabel = new JLabel();
@@ -114,62 +122,24 @@ public class ProfileDialog extends JDialog
     headLabel.setHorizontalTextPosition(SwingConstants.CENTER);
     headLabel.setHorizontalAlignment(SwingConstants.LEFT);
     headLabel.setAlignmentY(Component.LEFT_ALIGNMENT);
+    headLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
     headLabel.setForeground(Color.black);
     headLabel.setFont(new Font("Dialog", Font.BOLD, 14));
     headLabel.setBorder(BorderFactory.createLoweredBevelBorder());
-    entryPanel.add(headLabel);
+    JPanel headPanel = new JPanel();
+    headPanel.setLayout(new FlowLayout());
+    headPanel.add(headLabel);
+    entryPanel.add(headPanel);
     entryPanel.add(Box.createVerticalStrut(8));
-
-    JLabel firstNameLabel = new JLabel();
-    JLabel lastNameLabel = new JLabel();
-    JLabel usernameLabel = new JLabel();
-    JLabel passwordLabel = new JLabel();
-    JLabel passwordLabel2 = new JLabel();
-    firstNameLabel.setText("First name: ");
-    lastNameLabel.setText("Last name: ");
-    usernameLabel.setText("Username: ");
-    passwordLabel.setText("Password: ");
-    passwordLabel2.setText("Password again: ");
-    firstNameLabel.setForeground(Color.black);
-    lastNameLabel.setForeground(Color.black);
-    usernameLabel.setForeground(Color.black);
-    passwordLabel.setForeground(Color.black);
-    passwordLabel2.setForeground(Color.black);
-    firstNameLabel.setFont(new Font("Dialog", Font.PLAIN, 12));
-    lastNameLabel.setFont(new Font("Dialog", Font.PLAIN, 12));
-    usernameLabel.setFont(new Font("Dialog", Font.PLAIN, 12));
-    passwordLabel.setFont(new Font("Dialog", Font.PLAIN, 12));
-    passwordLabel2.setFont(new Font("Dialog", Font.PLAIN, 12));
-    firstNameLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-    lastNameLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-    usernameLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-    passwordLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-    passwordLabel2.setHorizontalAlignment(SwingConstants.RIGHT);
-
-    firstNameField.setColumns(15);
-    lastNameField.setColumns(15);
-    usernameField.setColumns(15);
-    passwordField.setColumns(10);
-    passwordField2.setColumns(10);
 
     screenPanel = new JPanel();
     screenPanel.setBorder(BorderFactory.createEmptyBorder(8,8,8,8));
-    screenPanel.setBackground(Color.green);
-    GridBagLayout gridbag = new GridBagLayout();
-    screenPanel.setLayout(gridbag);
-    JLabel[] labels = {firstNameLabel, lastNameLabel, usernameLabel,
-                       passwordLabel, passwordLabel2};
-    JTextField[] textFields = {firstNameField, lastNameField, usernameField,
-                       passwordField, passwordField2};
-    addLabelTextRows(labels, textFields, gridbag, screenPanel);
-
     entryPanel.add(screenPanel);
     entryPanel.add(Box.createVerticalGlue());
     entryPanel.add(Box.createRigidArea(new Dimension(8,8)));
 
     JPanel buttonPanel = new JPanel();
     buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
-    buttonPanel.setBackground(Color.yellow);
     buttonPanel.add(Box.createHorizontalGlue());
     cancelButton.setText("Cancel");
     cancelButton.setActionCommand("Cancel");
@@ -181,8 +151,9 @@ public class ProfileDialog extends JDialog
     previousButton.setHorizontalTextPosition(SwingConstants.RIGHT);
     buttonPanel.add(previousButton);
     buttonPanel.add(Box.createHorizontalStrut(8));
-    nextButton = new JButton("Next", new ImageIcon( getClass().
-          getResource("/toolbarButtonGraphics/navigation/Forward16.gif")));
+    forwardIcon = new ImageIcon( getClass().
+          getResource("/toolbarButtonGraphics/navigation/Forward16.gif"));
+    nextButton = new JButton("Next", forwardIcon);
     nextButton.setHorizontalTextPosition(SwingConstants.LEFT);
     nextButton.setEnabled(true);
     buttonPanel.add(nextButton);
@@ -194,6 +165,17 @@ public class ProfileDialog extends JDialog
     getContentPane().add(Box.createHorizontalGlue());
     getContentPane().add(Box.createHorizontalStrut(8));
 
+    // Layout the first screen
+    layoutScreen();
+
+    // Size and center the frame
+    pack();
+    setResizable(false);
+    Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
+    Rectangle frameDim = getBounds();
+    setLocation((screenDim.width - frameDim.width) / 2 ,
+            (screenDim.height - frameDim.height) /2);
+
     // register a listener for ActionEvents
     ActionHandler myActionHandler = new ActionHandler();
     previousButton.addActionListener(myActionHandler);
@@ -201,10 +183,6 @@ public class ProfileDialog extends JDialog
     cancelButton.addActionListener(myActionHandler);
 
     config = new ConfigXML("lib/config.xml");
-    
-    if (container!=null) {
-      usernameField.setText(container.getUserName());
-    }
   }
 
   /**
@@ -216,7 +194,7 @@ public class ProfileDialog extends JDialog
     {
       Object object = event.getSource();
       if (object == previousButton) {
-        previosuButtonHandler(event);
+        previousButtonHandler(event);
       } else if (object == nextButton) {
         nextButtonHandler(event);
       } else if (object == cancelButton) {
@@ -228,10 +206,12 @@ public class ProfileDialog extends JDialog
   /**
    * Perform actions associated with the Previous button
    */
-  private void previosuButtonHandler(ActionEvent event)
+  private void previousButtonHandler(ActionEvent event)
   {
-    screenPanel.validate();
-    screenPanel.paint(screenPanel.getGraphics());
+    if (currentScreen > 0) {
+      currentScreen--;
+      layoutScreen();
+    }
   }
   
   /**
@@ -239,6 +219,12 @@ public class ProfileDialog extends JDialog
    */
   private void nextButtonHandler(ActionEvent event)
   {
+    if (currentScreen == numScreens-1) {
+      createProfile();
+    } else {
+      currentScreen++;
+      layoutScreen();
+    }
   }
 
   /**
@@ -276,4 +262,157 @@ public class ProfileDialog extends JDialog
       container.add(textFields[i]);
     }
   }
+
+  /** 
+   * Put the new components on the screen and redraw.
+   */
+  private void layoutScreen()
+  {
+    screenPanel.removeAll();
+
+    GridBagLayout gridbag = new GridBagLayout();
+    screenPanel.setLayout(gridbag);
+
+    if (0 == currentScreen) {
+      String helpText = "<html><p>Enter your first and last name, " +
+                        "a username of your choosing, your organization, " +
+                        "your desired password, and " +
+                        "the same password again (for verification).</p>" +
+                        "<p>This information will be used to store your " +
+                        "data on your computer in a location that is " +
+                        "distinct from other Morpho users.</p></html>";
+      helpLabel.setText(helpText);
+
+      screenPanel.setBorder(BorderFactory.createTitledBorder(
+                            BorderFactory.createEmptyBorder(8,8,8,8),
+                            "Basic Information"));
+      JLabel firstNameLabel = new JLabel();
+      JLabel lastNameLabel = new JLabel();
+      JLabel usernameLabel = new JLabel();
+      JLabel passwordLabel = new JLabel();
+      JLabel passwordLabel2 = new JLabel();
+      firstNameLabel.setText("First name: ");
+      lastNameLabel.setText("Last name: ");
+      usernameLabel.setText("Username: ");
+      passwordLabel.setText("Password: ");
+      passwordLabel2.setText("Password again: ");
+      firstNameLabel.setForeground(Color.black);
+      lastNameLabel.setForeground(Color.black);
+      usernameLabel.setForeground(Color.black);
+      passwordLabel.setForeground(Color.black);
+      passwordLabel2.setForeground(Color.black);
+      firstNameLabel.setFont(new Font("Dialog", Font.PLAIN, 12));
+      lastNameLabel.setFont(new Font("Dialog", Font.PLAIN, 12));
+      usernameLabel.setFont(new Font("Dialog", Font.PLAIN, 12));
+      passwordLabel.setFont(new Font("Dialog", Font.PLAIN, 12));
+      passwordLabel2.setFont(new Font("Dialog", Font.PLAIN, 12));
+      firstNameLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+      lastNameLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+      usernameLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+      passwordLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+      passwordLabel2.setHorizontalAlignment(SwingConstants.RIGHT);
+  
+      firstNameField.setColumns(15);
+      lastNameField.setColumns(15);
+      usernameField.setColumns(15);
+      passwordField.setColumns(10);
+      passwordField2.setColumns(10);
+  
+      JLabel[] labels = {firstNameLabel, lastNameLabel, usernameLabel,
+                         passwordLabel, passwordLabel2};
+      JTextField[] textFields = {firstNameField, lastNameField, usernameField,
+                         passwordField, passwordField2};
+      addLabelTextRows(labels, textFields, gridbag, screenPanel);
+    } else if (1 == currentScreen) {
+      String helpText = "<html>Enter your contact information.  This " +
+                        "data will be used to pre-fill in metadata " +
+                        "fields when you are entering data, and so will " +
+                        "increase entry efficiency.</html>";
+      helpLabel.setText(helpText);
+      screenPanel.setBorder(BorderFactory.createTitledBorder(
+                            BorderFactory.createEmptyBorder(8,8,8,8),
+                            "Contact Information"));
+      JLabel constructionLabel = new JLabel("Not yet implemented.");
+      constructionLabel.setForeground(Color.black);
+      constructionLabel.setFont(new Font("Dialog", Font.PLAIN, 12));
+      constructionField.setColumns(15);
+      JLabel[] labels = {constructionLabel};
+      JTextField[] textFields = {constructionField};
+      addLabelTextRows(labels, textFields, gridbag, screenPanel);
+    }
+
+    // Set button states properly
+    if (0 == currentScreen) {
+      previousButton.setEnabled(false);
+    } else {
+      previousButton.setEnabled(true);
+    }
+
+    if (currentScreen == numScreens-1) {
+      nextButton.setEnabled(true);
+      nextButton.setText("Finished");
+      nextButton.setIcon(null);
+    } else {
+      nextButton.setEnabled(true);
+      nextButton.setText("Next");
+      nextButton.setIcon(forwardIcon);
+    }
+
+    // Repaint the screen
+    screenPanel.validate();
+    screenPanel.paint(screenPanel.getGraphics());
+  }
+
+  /**
+   * Validate the contents of required and critical fields before actually
+   * creating the profile.  If there are problems, return false, otherwise true
+   *
+   * @returns boolean true if fields are valid, false otherwise
+   */
+  private boolean validateFieldContents()
+  {
+    boolean fieldsAreValid = true;
+    if (usernameField.getText() == null || 
+        usernameField.getText().equals("")) {
+      fieldsAreValid = false;
+    }
+
+    if (!passwordField.getText().equals(passwordField2.getText())) {
+      fieldsAreValid = false;
+    }
+    return fieldsAreValid;
+  }
+
+  /**
+   * Process the data and create a new profile.
+   */
+  private void createProfile()
+  {
+    if (validateFieldContents()) {
+      // Create a profile directory
+      String profiledir = config.get("profile_directory", 0);
+      String profilePath = profiledir + File.separator + 
+                           usernameField.getText();
+      File profileDir = new File(profilePath);
+      if (!profileDir.mkdir()) {
+        // Error creating the directory
+      } else {
+        // Copy default options to that directory
+
+        // Create a metacat user 
+
+        // Get rid of the dialog
+        setVisible(false);
+        dispose();
+      }
+    } else {
+      currentScreen = 0;
+      layoutScreen();
+      String messageText = "Some required information was invalid.\n\n" +
+                           "Please check that you have provided a\n" +
+                           "username and that the two passwords match.\n";
+      JOptionPane.showMessageDialog(this, messageText);      
+    }
+  }
+
 }
