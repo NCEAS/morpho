acdaaa354455b82e233f6f4d8bc1221ebcb371f0
Matt Jones <jones@nceas.ucsb.edu>
2001-07-19 18:28:44 +0000

    Fix to the batch files that references the correct jar file for HTTPClient (httpclient.jar).

--- morpho.bat
+++ morpho.bat
@@ -1,1 +1,1 @@
-java -cp .;lib/xerces.jar;lib/xalan.jar;lib/morpho.jar;lib/jlfgr-1_0.jar;lib/HTTPClient-bin.jar -Djava.protocol.handler.pkgs=HTTPClient edu.ucsb.nceas.morpho.framework.ClientFramework
+java -cp .;lib/xerces.jar;lib/xalan.jar;lib/morpho.jar;lib/jlfgr-1_0.jar;lib/httpclient.jar -Djava.protocol.handler.pkgs=HTTPClient edu.ucsb.nceas.morpho.framework.ClientFramework
