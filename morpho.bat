2d4e2482318174ddc4b8ef20fb9b79316fb32b02
Dan Higgins <higgins@nceas.ucsb.edu>
2002-02-19 21:50:14 +0000

    ask for 128M of memory rather than default of 64M

--- morpho.bat
+++ morpho.bat
@@ -1,1 +1,1 @@
-java -cp .;lib/xerces.jar;lib/xalan.jar;lib/morpho.jar;lib/jlfgr-1_0.jar;lib/httpclient.jar;lib/itislib.jar -Djava.protocol.handler.pkgs=HTTPClient edu.ucsb.nceas.morpho.framework.ClientFramework
+java -Xmx128m -cp .;lib/xerces.jar;lib/xalan.jar;lib/morpho.jar;lib/jlfgr-1_0.jar;lib/httpclient.jar;lib/itislib.jar -Djava.protocol.handler.pkgs=HTTPClient edu.ucsb.nceas.morpho.framework.ClientFramework
