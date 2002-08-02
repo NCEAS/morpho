7e3e6df28927ac49cedd710c1b81e95d9ea8acdc
Jing Tao <tao@nceas.ucsb.edu>
2002-08-02 23:12:51 +0000

    Header renderer for sortable table.

---
+++ src/edu/ucsb/nceas/morpho/util/SortableTableHeaderCellRenderer.java
@@ -0,0 +1,92 @@
+/**
+ *  '$RCSfile: SortableTableHeaderCellRenderer.java,v $'
+ *  Copyright: 2000 Regents of the University of California and the
+ *              National Center for Ecological Analysis and Synthesis
+ *    Authors: @authors@
+ *    Release: @release@
+ *
+ *   '$Author: tao $'
+ *     '$Date: 2002-08-02 23:12:51 $'
+ * '$Revision: 1.1 $'
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package edu.ucsb.nceas.morpho.util;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.table.*;
+
+/**
+ * The class for Renderer of sortable table header cell
+ */
+public class SortableTableHeaderCellRenderer extends DefaultTableCellRenderer
+{
+  // The icon for nonsorted in table header cell
+  private Icon NONSORTED = null;
+   /* new SortArrowIcon(SortArrowIcon.NONE);*/
+  // The icon for ascending order in table header cell
+  private Icon ASCENDING = new ImageIcon 
+                      (getClass().getResource("images/ascendingArrow.gif"));
+  // The icon for decending order in table header cell
+  private Icon DECENDING = new ImageIcon 
+                      (getClass().getResource("images/decendingArrow.gif"));
+ 
+  /**
+   * Consturctor of SortableHeaderCellRender
+   */
+  /*public SortableTableHeaderCellRenderer()
+  {
+    setHorizontalTextPosition(LEFT);
+    setHorizontalAlignment(CENTER);
+  }*/
+  
+  /**
+   * Method to return table cell render
+   * @param table the JTable
+   * @param value the value to assign to the cell
+   * @param isSelected true if the cell is selected
+   * @param isFocus true if the cell has focus
+   * @param row the row of the cell to render
+   * @param the column of the cell to render
+   */
+  public Component getTableCellRendererComponent( JTable table, Object value, 
+                          boolean isSelected,boolean hasFocus, int row, int col)
+  {
+    int index = -1;
+    boolean ascending = true;
+    if (table instanceof SortableJTable)
+    {
+      SortableJTable sortTable = (SortableJTable)table;
+      index = sortTable.getSortedColumnIndex();
+      ascending = sortTable.isSortedColumnAscending();
+    }
+    if (table != null)
+    {
+      JTableHeader header = table.getTableHeader();
+      if (header != null)
+      {
+        setForeground(header.getForeground());
+        setBackground(header.getBackground());
+        setFont(header.getFont());
+      }
+    }
+    Icon icon = ascending ? ASCENDING : DECENDING;
+    setIcon(col == index ? icon : NONSORTED);
+    setText((value == null) ? "" : value.toString());
+    setBorder(UIManager.getBorder("TableHeader.cellBorder"));
+    return this;
+  }
+}
+
