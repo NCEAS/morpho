0a1864d32940f9658b32ab626158e423a814a537
Matthew Brooke <brooke@nceas.ucsb.edu>
2002-09-11 21:56:15 +0000

    added tests for new methods that accept DOM Document parameters. Also had to add other code to prepare those test DOM docs, ready to be used in tests

--- tests/edu/ucsb/nceas/morphotest/util/XMLTransformerTest.java
+++ tests/edu/ucsb/nceas/morphotest/util/XMLTransformerTest.java
@@ -6,8 +6,8 @@
  *    Release: @release@
  *
  *   '$Author: brooke $'
- *     '$Date: 2002-09-06 23:10:49 $'
- * '$Revision: 1.6 $'
+ *     '$Date: 2002-09-11 21:56:15 $'
+ * '$Revision: 1.7 $'
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,19 +26,41 @@
 
 package edu.ucsb.nceas.morphotest.util;
 
+import edu.ucsb.nceas.morpho.Morpho;
+import edu.ucsb.nceas.morpho.framework.ConfigXML;
+import edu.ucsb.nceas.morpho.util.IOUtil;
+import edu.ucsb.nceas.morpho.util.XMLTransformer;
+
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.IOException;
+
+import java.net.URL;
+import java.net.MalformedURLException;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import edu.ucsb.nceas.morpho.util.IOUtil;
-import edu.ucsb.nceas.morpho.util.XMLTransformer;
 
 import edu.ucsb.nceas.morpho.util.Log;
 
+import org.w3c.dom.Document;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXParseException;
+
+import org.xml.sax.EntityResolver;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;     
+
+import com.arbortext.catalog.Catalog;
+import com.arbortext.catalog.CatalogEntityResolver;
 
 /**
  * A JUnit test for testing the metadisplay plugin.
@@ -47,6 +69,8 @@ public class XMLTransformerTest extends TestCase
 {
 
   private static Exception testException;
+  private final String CONFIG_KEY_LOCAL_CATALOG_PATH  = "local_catalog_path";
+  private ConfigXML config = Morpho.getConfiguration();
 
   /**
    * Constructor to build the test
@@ -107,7 +131,7 @@ public class XMLTransformerTest extends TestCase
         
         System.out.println("testing with null XML reader..."); 
         try {
-            result = transformer.transform(null, getXSL_testDoc());
+            result = transformer.transform((Reader)null, getXSL_testDoc());
         } catch (IOException e) {
             System.out.println("OK: testTransform() IOException: " 
                                                             + e.getMessage());
@@ -133,26 +157,100 @@ public class XMLTransformerTest extends TestCase
         testException = null;
         result = null;
 
-        for (int i=0;i<10 ;i++) {
-            System.out.println("test #"+i+" with proper XML *and* XSL reader..."); 
-            try {
-                result = transformer.transform(getXML_testDoc(), getXSL_testDoc());
-            } catch (IOException e) {
-                e.printStackTrace();
-                fail("Unexpected testTransform() IOException: " + e.getMessage());
-            }
-            assertNotNull(result);
-            try {
-                assertEquals(
-                    IOUtil.getAsStringBuffer(getHTML_resultDoc(),true).toString(),
-                    IOUtil.getAsStringBuffer(result,true).toString() );
-            } catch (IOException e) {
-                e.printStackTrace();
-                fail("IOException trying to read returned Readers "+e);
-            }
-            testException = null;
-            result = null;
+        
+        System.out.println("testing with null XML DOM Document..."); 
+        try {
+            result = transformer.transform((Document)null, getXSL_testDoc());
+        } catch (IOException e) {
+            System.out.println("OK: testTransform() IOException: " 
+                                                            + e.getMessage());
+            testException = e;
         }
+        assertNotNull(testException);
+        assertNull(result);
+        
+        testException = null;
+        result = null;
+        
+        
+        System.out.println("testing with XML Reader only, should use generic stylesheet"); 
+        try {
+            result = transformer.transform(getXML_testDoc());
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Unexpected testTransform() IOException: " + e.getMessage());
+        }
+        assertNotNull(result);
+        try {
+            assertEquals(
+                IOUtil.getAsStringBuffer(getHTML_generic_resultDoc(),true).toString(),
+                IOUtil.getAsStringBuffer(result,true).toString() );
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("IOException trying to read returned Readers "+e);
+        }
+        testException = null;
+        result = null;
+
+        System.out.println("testing with XML Reader and XSL Reader..."); 
+        try {
+            result = transformer.transform(getXML_testDoc(), getXSL_testDoc());
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Unexpected testTransform() IOException: " + e.getMessage());
+        }
+        assertNotNull(result);
+        try {
+            assertEquals(
+                IOUtil.getAsStringBuffer(getHTML_resultDoc(),true).toString(),
+                IOUtil.getAsStringBuffer(result,true).toString() );
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("IOException trying to read returned Readers "+e);
+        }
+        testException = null;
+        result = null;
+
+      System.out.println("testing with XML DOM Document only, should use generic stylesheet"); 
+      try {
+          result = transformer.transform(getAsDOMDocument(getXML_testDoc()));
+      } catch (IOException e) {
+          e.printStackTrace();
+          fail("Unexpected testTransform() IOException: " + e.getMessage());
+      }
+      assertNotNull(result);
+      try {
+          assertEquals(
+              IOUtil.getAsStringBuffer(getHTML_generic_resultDoc(),true).toString(),
+              IOUtil.getAsStringBuffer(result,true).toString() );
+      } catch (IOException e) {
+          e.printStackTrace();
+          fail("IOException trying to read returned Readers "+e);
+      }
+      testException = null;
+      result = null;
+      
+      System.out.println("testing with XML DOM Document and XSL Reader..."); 
+      try {
+          result = transformer.transform(getAsDOMDocument(getXML_testDoc()), getXSL_testDoc());
+      } catch (IOException e) {
+          e.printStackTrace();
+          fail("Unexpected testTransform() IOException: " + e.getMessage());
+      }
+      assertNotNull(result);
+      try {
+          assertEquals(
+              IOUtil.getAsStringBuffer(getHTML_resultDoc(),true).toString(),
+              IOUtil.getAsStringBuffer(result,true).toString() );
+      } catch (IOException e) {
+          e.printStackTrace();
+          fail("IOException trying to read returned Readers "+e);
+      }
+      testException = null;
+      result = null;
+
+      
+
     }
 
 
@@ -162,7 +260,68 @@ public class XMLTransformerTest extends TestCase
         System.exit(0);
     }
 
+    //  Create a DOM Document to enable the transformer to access the Reader
+    //  - Necessary because we need to set the entity resolver for this source, 
+    //  which isn't possible if we just use a StreamSource to read the Reader
+    private synchronized Document getAsDOMDocument(Reader xmlDocReader) 
+    {
+        Document doc = null;
+        
+        try {
+            InputSource source = new InputSource(xmlDocReader);
 
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(true);
+            factory.setValidating(true);
+        
+            DocumentBuilder docBuilder = factory.newDocumentBuilder();
+            docBuilder.setEntityResolver(getEntityResolver());
+            docBuilder.setErrorHandler(new CustomErrorHandler());
+        
+            doc = docBuilder.parse(source);
+            
+        } catch (IOException e) {
+            lazyThrow(e, "IOException");
+        } catch (SAXException e) {
+            lazyThrow(e, "SAXException");
+        } catch (FactoryConfigurationError e) {
+            lazyThrow(e, "FactoryConfigurationError");
+        } catch (ParserConfigurationException e) {
+            lazyThrow(e, "ParserConfigurationException");
+        }
+        return doc;
+    }
+    
+    //get entity resolver instance
+    private EntityResolver getEntityResolver() 
+    {
+        CatalogEntityResolver catalogEntResolver = new CatalogEntityResolver();
+        try {
+            Catalog catalog = new Catalog();
+            catalog.loadSystemCatalogs();
+            String catalogPath = config.get(CONFIG_KEY_LOCAL_CATALOG_PATH, 0);
+            ClassLoader cl = Thread.currentThread().getContextClassLoader();
+            URL catalogURL = cl.getResource(catalogPath);
+
+            catalog.parseCatalog(catalogURL.toString());
+            catalogEntResolver.setCatalog(catalog);
+        } catch (Exception e) {
+            Log.debug(9,"XMLTransformer: error creating Catalog "+e.toString());
+        }
+        return (EntityResolver)catalogEntResolver;
+    }
+    
+    private void lazyThrow(Throwable e, String type)
+    {
+        String msg 
+            = "\nXMLTransformer.doTransform(): getting DOM document. "
+                +"Nested "+type+" = "+e.getMessage()+"\n";
+        e.fillInStackTrace();
+        e.printStackTrace();
+        Log.debug(12, msg);
+        fail("Unexpected "+type+e.getMessage());
+    }
+    
     private Reader getXML_testDoc()  {
         return new StringReader(
               "<?xml version=\"1.0\"?>"
@@ -211,7 +370,6 @@ public class XMLTransformerTest extends TestCase
             +"</xsl:stylesheet>");
     }
     
-
     private static final Reader getHTML_resultDoc() {
         return new StringReader(
              "<html>\r\n"
@@ -227,6 +385,58 @@ public class XMLTransformerTest extends TestCase
             +"</body>\r\n"
             +"</html>\r\n");
     }
+    
+    private static final Reader getHTML_generic_resultDoc() {
+        return new StringReader(
+             "<html>\r\n"
+            +"<head>\r\n"
+            +"<META http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">\r\n"
+            +"</head>\r\n"
+            +"<body>\r\n"
+            +"<center>\r\n"
+            +"<h1>Ecological Metadata Language</h1>\r\n"
+            +"</center>\r\n"
+            +"<table align=\"center\" cellpadding=\"5\" border=\"1\" width=\"90%\">\r\n"
+            +"<tr>\r\n"
+            +"<td><b>Element Name</b></td><td><b>Value</b></td>\r\n"
+            +"</tr>\r\n"
+            +"<tr>\r\n"
+            +"<td>eml-attribute</td><td>TESTDOC_1  field 1  label for attribute 1  none whatsoever  cm  integer                2        222          ~  5</td>\r\n"
+            +"</tr>\r\n"
+            +"</table>\r\n"
+            +"</body>\r\n"
+            +"</html>\r\n");
+    }
+
+   
+    
+    class CustomErrorHandler implements ErrorHandler
+    {   
+        public void error(SAXParseException exception) throws SAXException
+        {
+            handleException(exception, "SAX ERROR");
+        }
+        
+        public void fatalError(SAXParseException exception) throws SAXException 
+        {
+            handleException(exception, "SAX FATAL ERROR");
+        }
+        
+        public void warning(SAXParseException exception) throws SAXException 
+        {
+            handleException(exception, "SAX WARNING");
+        }
+        
+        private void handleException(SAXParseException exception,
+                                        String errMessage) throws SAXException
+        {
+            Log.debug(9, "XMLTransformer$CustomErrorHandler: " 
+                                    + errMessage+": " + exception.getMessage());
+            exception.fillInStackTrace();
+            throw ((SAXException)exception);
+        }
+    }
+    
 }
 
 
