1fdb6504e1371694baa323fc9aa99e76f9c680fc
Matthew Brooke <brooke@nceas.ucsb.edu>
2002-09-24 17:14:33 +0000

    command and classpath previously all on one l-o-n-g line - now formatted same as linux shell script for easier reading/debugging/experimentation

--- morpho.bat
+++ morpho.bat
@@ -1,1 +1,18 @@
-java -Xmx512m -Xss1m -cp .;lib/xercesImpl.jar;lib/xmlParserAPIs.jar;lib/xalan.jar;lib/kunststoff.jar;lib/morpho.jar;lib/morpho-config.jar;lib/jlfgr-1_0.jar;lib/httpclient.jar;lib/itislib.jar;lib/jsse.jar;lib/jcert.jar;lib/jnet.jar edu.ucsb.nceas.morpho.Morpho
+
+SET  XMLP=lib\xercesImpl.jar
+SET   API=lib\xml-apis.jar
+# SET   API=lib\xmlParserAPIs.jar
+SET XALAN=lib\xalan.jar
+SET   DMC=lib\morpho.jar
+SET   CFG=lib\morpho-config.jar
+SET   JLF=lib\jlfgr-1_0.jar
+SET   HTP=lib\httpclient.jar
+SET   ITS=lib\itislib.jar
+SET   JSE=lib\jsse.jar
+SET   JCE=lib\jcert.jar
+SET   JNE=lib\jnet.jar
+SET   LAF=lib\kunststoff.jar
+
+SET CPATH=.;%XMLP%;%API%;%XALAN%;%DMC%;%CFG%;%JLF%;%HTP%;%ITS%;%JSE%;%JCE%;%JNE%;%LAF%
+
+java -Xmx512m -Xss1m -cp %CPATH% edu.ucsb.nceas.morpho.Morpho
