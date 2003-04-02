432592950cd97b85a7ef01399be3258aa5162dbd
Dan Higgins <higgins@nceas.ucsb.edu>
2003-04-02 18:46:12 +0000

    new utility class; created to centralize the 'normalize()' method which removes invalid characters from strings that will be placed inside XML tags. Previously, the normalize method was repeated in 8 different classes.

---
+++ src/edu/ucsb/nceas/morpho/util/XMLUtil.java
@@ -0,0 +1,85 @@
+/**
+ *  '$RCSfile: XMLUtil.java,v $'
+ *  Copyright: 2000 Regents of the University of California and the
+ *              National Center for Ecological Analysis and Synthesis
+ *    Authors: @authors@
+ *    Release: @release@
+ *
+ *   '$Author: higgins $'
+ *     '$Date: 2003-04-02 18:46:12 $'
+ * '$Revision: 1.1 $'
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.ucsb.nceas.morpho.util;
+
+
+public class XMLUtil                                         
+{
+    /** Normalizes the given string. */
+    public static String normalize(Object ss) {
+        String s = "";
+        s = (String)ss;
+        StringBuffer str = new StringBuffer();
+
+        int len = (s != null) ? s.length() : 0;
+        for (int i = 0; i < len; i++) {
+            char ch = s.charAt(i);
+            switch (ch) {
+                case '<': {
+                    str.append("&lt;");
+                    break;
+                }
+                case '>': {
+                    str.append("&gt;");
+                    break;
+                }
+                case '&': {
+                    str.append("&amp;");
+                    break;
+                }
+                case '"': {
+                    str.append("&quot;");
+                    break;
+                }
+                case '\r':
+		            case '\t':
+                case '\n': {
+                    if (false) {
+                        str.append("&#");
+                        str.append(Integer.toString(ch));
+                        str.append(';');
+                        break;
+                    }
+                    // else, default append char
+			              break;
+                }
+                default: {
+                    if (ch<128) {
+                      str.append(ch);
+                    } else {
+                        str.append("&#");
+                        str.append(Integer.toString(ch));
+                        str.append(';');
+                    }
+                }
+            }
+        }
+
+        return str.toString();
+
+    } // normalize(String):String
+}
