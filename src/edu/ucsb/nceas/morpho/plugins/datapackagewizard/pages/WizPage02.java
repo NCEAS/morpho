b4ee0da8803d5a245851944018483fb6f9c86441
Matthew Brooke <brooke@nceas.ucsb.edu>
2003-07-28 19:09:08 +0000

    stubs for new pages

---
+++ src/edu/ucsb/nceas/morpho/plugins/datapackagewizard/pages/WizPage02.java
@@ -0,0 +1,119 @@
+/**
+ *  '$RCSfile: WizPage02.java,v $'
+ *    Purpose: A class that handles xml messages passed by the 
+ *             package wizard
+ *  Copyright: 2000 Regents of the University of California and the
+ *             National Center for Ecological Analysis and Synthesis
+ *    Authors: Chad Berkley
+ *    Release: @release@
+ *
+ *   '$Author: brooke $'
+ *     '$Date: 2003-07-28 19:09:08 $'
+ * '$Revision: 1.1 $'
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.ucsb.nceas.morpho.plugins.datapackagewizard.pages;
+
+import java.util.Map;
+
+import edu.ucsb.nceas.morpho.plugins.datapackagewizard.AbstractWizardPage;
+import edu.ucsb.nceas.morpho.plugins.datapackagewizard.WizardPageLibrary;
+
+
+public class WizPage02 extends AbstractWizardPage{
+
+  public final String pageID     = WizardPageLibrary.PAGE02_ID;
+  public final String nextPageID = null;
+  public final String title      = "Welcome to page 2 of the Data Package Wizard";
+  public final String subtitle   = "Please read the instructions below before proceeding";
+  
+
+
+  public WizPage02() {
+
+    this.setBackground(java.awt.Color.blue);
+  }
+  
+  /** 
+   *  The action to be executed when the page is displayed. May be empty
+   */
+  public void onLoadAction() {
+    
+  }
+
+  /** 
+   *  The action to be executed when the "Prev" button is pressed. May be empty
+   *
+   */
+  public void onRewindAction() {
+    
+  }
+  
+  
+  /** 
+   *  The action to be executed when the "Next" button (pages 1 to last-but-one)
+   *  or "Finish" button(last page) is pressed. May be empty
+   */
+  public void onAdvanceAction() {
+    
+  }
+  
+
+  /** 
+   *  gets the Map object that contains all the key/value paired
+   *  settings for this particular wizard page
+   *
+   *  @return   data the Map object that contains all the
+   *            key/value paired settings for this particular wizard page
+   */
+  public Map getPageData() {
+  
+    return null;
+  }
+
+  /** 
+   *  gets the unique ID for this wizard page
+   *
+   *  @return   the unique ID String for this wizard page
+   */
+  public String getPageID() { return pageID; }
+
+  /** 
+   *  gets the title for this wizard page
+   *
+   *  @return   the String title for this wizard page
+   */
+  public String getTitle() { return title; }
+  
+  /** 
+   *  gets the subtitle for this wizard page
+   *
+   *  @return   the String subtitle for this wizard page
+   */
+  public String getSubtitle() { return subtitle; }
+  
+  /** 
+   *  Returns the ID of the page that the user will see next, after the "Next" 
+   *  button is pressed. If this is the last page, return value must be null
+   *
+   *  @return the String ID of the page that the user will see next, or null if 
+   *  this is te last page
+   */
+  public String getNextPageID() { return nextPageID; }
+
+  
+}
