b5aebdb80894564eb2239690f52650815a2142ba
Dan Higgins <higgins@nceas.ucsb.edu>
2003-08-08 16:48:50 +0000

    new editor version

--- src/edu/ucsb/nceas/morpho/editor/XMLDisplayHandler.java
+++ src/edu/ucsb/nceas/morpho/editor/XMLDisplayHandler.java
@@ -6,8 +6,8 @@
  *    Release: @release@
  *
  *   '$Author: higgins $'
- *     '$Date: 2001-12-07 16:58:36 $'
- * '$Revision: 1.12 $'
+ *     '$Date: 2003-08-08 16:48:50 $'
+ * '$Revision: 1.12.18.1 $'
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -88,9 +88,24 @@ class XMLDisplayHandler extends DefaultHandler implements LexicalHandler {
 		//  Create new Node
 		NodeInfo ni = new NodeInfo(localName);
 		for (int i=0;i<atts.getLength();i++) {
+      if (atts.getLocalName(i).equals("help")) {
+        ni.setHelp(atts.getValue(i));
+      }
+      else if(atts.getLocalName(i).equals("cardinality")) {
+        ni.setCardinality(atts.getValue(i));
+      }
+      else if(atts.getLocalName(i).equals("nodeVisLevel")) {
+        String visLevel = atts.getValue(i);
+        int nvl = (new Integer(visLevel)).intValue();
+        ni.setNodeVisLevel(nvl);
+      }
+      else {
 		    ni.attr.put(atts.getLocalName(i),atts.getValue(i));
+//        System.out.println("key:"+atts.getLocalName(i)+"   value:"+atts.getValue(i));
+      }
 		}
-		DefaultMutableTreeNode newNode = new DefaultMutableTreeNode (ni);
+    setCardinality(ni);
+		DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(ni);
 		if (nodeCount>0) {	
 		  //  Add current Node to Node on top of Stack
 		  DefaultMutableTreeNode parentNode = (DefaultMutableTreeNode) stack.peek();
@@ -106,6 +121,26 @@ class XMLDisplayHandler extends DefaultHandler implements LexicalHandler {
 		stack.push (newNode);
 		
     }
+    
+    /*
+     * handle case where cardinality is stored in attributes called
+     * 'minOccurs' and 'maxOccurs'
+     *
+     */
+    private void setCardinality(NodeInfo ni) {
+      String minOccurs = "";
+      String maxOccurs = "";
+		  if (ni.attr.containsKey("minOccurs")) {
+        minOccurs = (String)ni.attr.get("minOccurs");
+      }
+      if (ni.attr.containsKey("maxOccurs")) {
+        maxOccurs = (String)ni.attr.get("maxOccurs");        
+      }
+      if ((minOccurs.equals("1")) && (maxOccurs.equals("1"))) ni.setCardinality("ONE");
+      if ((minOccurs.equals("0")) && (maxOccurs.equals("1"))) ni.setCardinality("OPTIONAL");
+      if ((minOccurs.equals("1")) && (!maxOccurs.equals("1"))) ni.setCardinality("ONE to MANY");
+      if ((minOccurs.equals("0")) && (!maxOccurs.equals("1"))) ni.setCardinality("ZERO to MANY");
+    }
   
     public void endElement (String uri, String localName,
                             String qName) throws SAXException {
@@ -113,7 +148,7 @@ class XMLDisplayHandler extends DefaultHandler implements LexicalHandler {
      // a tree node in endelement due the possibility of text actually being returned in
      // several successive 'characters' methods rather than a single method invocation;
      // this technique allows the text to be concatenated into a single text node
-    	if (text.length()>0) {
+    	if (text.trim().length()>0) {
 		    NodeInfo ni = new NodeInfo("#PCDATA");
 		    DefaultMutableTreeNode newNode = new DefaultMutableTreeNode (ni);
 		    DefaultMutableTreeNode parentNode = (DefaultMutableTreeNode) stack.peek();
@@ -135,7 +170,11 @@ class XMLDisplayHandler extends DefaultHandler implements LexicalHandler {
      // this technique allows the text to be concatenated into a single text node
 		// Set Text of Node on top of Stack
     	text = text + new String (ch, start, length);
-    	text = text.trim();
+      // make sure that empty text nodes are not created
+      if (text.length()>1) {
+    	  text = text.trim();
+        if (text.equals("")) text = " ";
+      }
     	text = text.replace('\n', ' ');
     }
 
