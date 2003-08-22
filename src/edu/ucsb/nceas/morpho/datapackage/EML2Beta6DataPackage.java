c14e387c42819e52dbae4ac777013f0e1d2820fc
Dan Higgins <higgins@nceas.ucsb.edu>
2003-08-22 18:55:35 +0000

    stub class for beta6 datapackages. (Currently does nothing)

---
+++ src/edu/ucsb/nceas/morpho/datapackage/EML2Beta6DataPackage.java
@@ -0,0 +1,37 @@
+/**
+ *  '$RCSfile: EML2Beta6DataPackage.java,v $'
+ *  Copyright: 2000 Regents of the University of California and the
+ *              National Center for Ecological Analysis and Synthesis
+ *    Authors: @authors@
+ *    Release: @release@
+ *
+ *   '$Author: higgins $'
+ *     '$Date: 2003-08-22 18:55:35 $'
+ * '$Revision: 1.1 $'
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.ucsb.nceas.morpho.datapackage;
+
+/**
+ * class that represents a data package. This class is abstract. Specific datapackages
+ * e.g. eml2, beta6., etc extend this abstact class
+ */
+public  class EML2Beta6DataPackage extends AbstractDataPackage
+{
+
+}
+
