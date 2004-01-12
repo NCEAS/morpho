6d60a511d9be6c78182399b234a4b9ca687028b6
Dan Higgins <higgins@nceas.ucsb.edu>
2004-01-12 23:07:33 +0000

    preliminary widgets added for setting geographic information using map display.

--- src/edu/ucsb/nceas/morpho/plugins/datapackagewizard/pages/Geographic.java
+++ src/edu/ucsb/nceas/morpho/plugins/datapackagewizard/pages/Geographic.java
@@ -7,9 +7,9 @@
  *    Authors: Saurabh Garg
  *    Release: @release@
  *
- *   '$Author: brooke $'
- *     '$Date: 2004-01-07 02:02:18 $'
- * '$Revision: 1.3 $'
+ *   '$Author: higgins $'
+ *     '$Date: 2004-01-12 23:07:33 $'
+ * '$Revision: 1.4 $'
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -28,11 +28,27 @@
 
 package edu.ucsb.nceas.morpho.plugins.datapackagewizard.pages;
 
+import edu.ucsb.nceas.morpho.plugins.datapackagewizard.WidgetFactory;
 import edu.ucsb.nceas.morpho.plugins.DataPackageWizardInterface;
 import edu.ucsb.nceas.morpho.plugins.datapackagewizard.AbstractWizardPage;
+import edu.ucsb.nceas.morpho.plugins.datapackagewizard.WizardSettings;
+
 import edu.ucsb.nceas.utilities.OrderedMap;
+import edu.ucsb.nceas.morpho.query.LiveMapPanel;
+import edu.ucsb.nceas.morpho.util.Log;
+
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.*;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class Geographic extends AbstractWizardPage {
 
@@ -43,6 +59,11 @@ public class Geographic extends AbstractWizardPage {
 
   public final String title      = "Geographic Information";
   public final String subtitle   = "Unintentionally left blank";
+  
+  private JTextArea   covDescField;
+  private JLabel regionSelectionLabel;
+  private JList regionList;
+  private LiveMapPanel lmp;
 
 
   public Geographic() {
@@ -55,9 +76,108 @@ public class Geographic extends AbstractWizardPage {
    are displayed only in this frame (doesn't include prev/next buttons etc)
    */
   private void init() {
+    this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+    JPanel vbox = this;
 
-    this.setLayout(new BorderLayout());
+    vbox.add(WidgetFactory.makeHalfSpacer());
 
+    JLabel coverageDesc = WidgetFactory.makeHTMLLabel(
+        "<b>Enter a description of the geographic coverage.</b> This provides a "
+       +"textual description about the area relevent to the data", 2);
+    vbox.add(coverageDesc);
+
+    JPanel covDescPanel = WidgetFactory.makePanel();
+
+    JLabel covDescLabel = WidgetFactory.makeLabel(" Description:", true);
+    covDescLabel.setVerticalAlignment(SwingConstants.TOP);
+    covDescLabel.setAlignmentY(SwingConstants.TOP);
+    covDescPanel.add(covDescLabel);
+
+    covDescField = WidgetFactory.makeTextArea("", 6, true);
+    JScrollPane jscrl = new JScrollPane(covDescField);
+    covDescPanel.add(jscrl);
+
+    covDescPanel.setBorder(new javax.swing.border.EmptyBorder(0,0,0,5*WizardSettings.PADDING));
+    vbox.add(covDescPanel);
+
+    vbox.add(WidgetFactory.makeDefaultSpacer());
+
+    JLabel bbDesc = WidgetFactory.makeHTMLLabel(
+        "<b>Set the geographic coordinates which bound the coverage</b> Latitude and longitude"
+       +"values are used to create a 'bounding box' containing the region of interest. "
+       +"Drag or click on the map. Then edit the text boxes if necessary. "
+       +"[Default entries are in fractional degrees. To enter in degreea/minutes/seconds, simply "
+       +"type a space between the degrees, minutes, and seconds values]", 4);
+    vbox.add(bbDesc);
+
+
+    JPanel bboxPanel = WidgetFactory.makePanel();
+    
+    JLabel bboxLabel = WidgetFactory.makeLabel("Bounding Box:", true);
+    bboxLabel.setVerticalAlignment(SwingConstants.TOP);
+    bboxLabel.setAlignmentY(SwingConstants.TOP);
+    bboxPanel.add(bboxLabel);
+    
+    lmp = new LiveMapPanel();
+    bboxPanel.add(lmp);
+
+    bboxPanel.setBorder(new javax.swing.border.EmptyBorder(0,0,0,5*WizardSettings.PADDING));
+    vbox.add(bboxPanel);
+
+  ////////////////////////////////////////////////////////////////////////////
+
+    JPanel regionPanel = new JPanel();
+    regionPanel.setLayout(new GridLayout(1,2));
+    JPanel regionSelectionPanel = WidgetFactory.makePanel(4);
+
+    regionSelectionLabel = WidgetFactory.makeLabel("Named Regions:", false, WizardSettings.WIZARD_CONTENT_LABEL_DIMS);
+    regionSelectionLabel.setVerticalAlignment(SwingConstants.TOP);
+    regionSelectionLabel.setAlignmentY(SwingConstants.TOP);
+    regionSelectionPanel.add(regionSelectionLabel);
+
+    String[] regions = new String[5];
+    regions[0] = "anywhere";
+    regions[1] = "region 1";
+    regions[2] = "region 2";
+    regions[3] = "region 3";
+    regions[4] = "region 4";
+    
+    
+    regionList = new JList(regions);
+    regionList.setFont(WizardSettings.WIZARD_CONTENT_FONT);
+    regionList.setForeground(WizardSettings.WIZARD_CONTENT_TEXT_COLOR);
+    regionList.setSelectedIndex(0);
+    JScrollPane jscr2 = new JScrollPane(regionList);
+    regionSelectionPanel.add(jscr2);
+    JLabel regionHelpLabel = getLabel("Click button to display region.");
+    JButton selectButton = new JButton("Select");
+    selectButton.setPreferredSize(new Dimension(60,24));
+    selectButton.setMaximumSize(new Dimension(60,24));
+    selectButton.setMargin(new Insets(0, 2, 1, 2));
+    selectButton.setEnabled(true);
+    selectButton.setFont(WizardSettings.WIZARD_CONTENT_FONT);
+    selectButton.setFocusPainted(false);
+    selectButton.addActionListener( new ActionListener() {
+      public void actionPerformed(ActionEvent ae) {
+        String selection = (String)regionList.getSelectedValue();
+        Log.debug(1,"Selection: "+selection);
+        lmp.setBoundingBox(45.0, -46.0, -47.0, 48.0);
+      }
+    });
+
+    JPanel buttonPanel = new JPanel();
+    buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+    buttonPanel.add(selectButton);
+    buttonPanel.add(regionHelpLabel);
+    
+    
+    regionPanel.add(regionSelectionPanel);
+    regionPanel.add(buttonPanel);
+
+    vbox.add(regionPanel);
+
+    vbox.add(WidgetFactory.makeDefaultSpacer());
+    
   }
 
 
@@ -142,4 +262,29 @@ public class Geographic extends AbstractWizardPage {
   public String getPageNumber() { return pageNumber; }
 
   public void setPageData(OrderedMap data) { }
+  
+  private JLabel getLabel(String text) {
+    if (text==null) text="";
+    JLabel label = new JLabel(text);
+
+    label.setAlignmentX(1.0f);
+    label.setFont(WizardSettings.WIZARD_CONTENT_FONT);
+    label.setBorder(BorderFactory.createMatteBorder(1,10,1,3, (Color)null));
+
+    return label;
+  }
+  
+    /**
+   *  This is a static main method configured to test the class 
+   */
+  static public void main(String args[]) {
+    JFrame frame = new JFrame("Demo/Test");
+    frame.setSize(800, 600);
+    frame.getContentPane().setLayout(new BorderLayout());
+    Geographic geo = new Geographic();
+    frame.getContentPane().add(geo, BorderLayout.CENTER);
+    frame.setVisible(true);
+
+  }
+
 }
