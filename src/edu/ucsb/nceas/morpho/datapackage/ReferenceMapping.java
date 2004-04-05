979ee1b1013c454434a5c469832a53b494504ac4
Jing Tao <tao@nceas.ucsb.edu>
2004-04-05 21:32:22 +0000

    Move this class from ReferenceHandler.

---
+++ src/edu/ucsb/nceas/morpho/datapackage/ReferenceMapping.java
@@ -0,0 +1,60 @@
+/**
+ *  '$RCSfile: ReferenceMapping.java,v $'
+ *  Copyright: 2000 Regents of the University of California and the
+ *              National Center for Ecological Analysis and Synthesis
+ *    Authors: @authors@
+ *    Release: @release@
+ *
+ *   '$Author: tao $'
+ *     '$Date: 2004-04-05 21:32:22 $'
+ * '$Revision: 1.1 $'
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.ucsb.nceas.morpho.datapackage;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: This calss is mapping of id and surrogate(description)</p>
+ * <p>Copyright: Copyright (c) 2004</p>
+ * <p>Company: </p>
+ * @author not attributable
+ * @version 1.0
+ */
+
+public class ReferenceMapping {
+
+  private String surrogate;
+  private String ID;
+
+  public ReferenceMapping(String ID, String surrogate) {
+
+    this.ID = ID;
+    this.surrogate = surrogate;
+  }
+
+  public String toString() {
+
+    return surrogate;
+  }
+
+
+  public String getID() {
+
+    return ID;
+  }
+
+}
