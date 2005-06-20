341cb4af3c5fd987abbce804d57c3273bba0e861
Saurabh Garg <garg@nceas.ucsb.edu>
2005-06-20 19:18:08 +0000

    Changing the position of menu items in File menu to add new 'Save Copy...' menuitem

--- src/edu/ucsb/nceas/morpho/plugins/printer/PrinterPlugin.java
+++ src/edu/ucsb/nceas/morpho/plugins/printer/PrinterPlugin.java
@@ -5,9 +5,9 @@
  *    Authors: @authors@
  *    Release: @release@
  *
- *   '$Author: sambasiv $'
- *     '$Date: 2004-04-23 22:43:05 $'
- * '$Revision: 1.5 $'
+ *   '$Author: sgarg $'
+ *     '$Date: 2005-06-20 19:18:08 $'
+ * '$Revision: 1.6 $'
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -52,35 +52,35 @@ import java.awt.Dimension;
 
 /**
  *  Plugin that provides a display panel to display HTML or plain text and
- *	allows the user to print the document 
+ *	allows the user to print the document
  */
- 
+
 public class PrinterPlugin implements       PrinterInterface,
 																						PluginInterface,
                                             ServiceProvider
 {
-	
+
 	private Morpho instanceOfMorpho;
 	private final ClassLoader classLoader;
-	
+
 	/** Constant to indicate a separator should precede an action */
 	private static String SEPARATOR_PRECEDING = "separator_preceding";
 	/** Constant to indicate a separator should follow an action */
 	private static String SEPARATOR_FOLLOWING = "separator_following";
-	
-	private Dimension PRINT_FRAME_DIMENSION = new Dimension(800, 600);  
+
+	private Dimension PRINT_FRAME_DIMENSION = new Dimension(800, 600);
 	private static PageFormat pageFormat = null;
-	
-	public PrinterPlugin() 
+
+	public PrinterPlugin()
 	 {
 		 	classLoader = Morpho.class.getClassLoader();
       Thread t = Thread.currentThread();
       t.setContextClassLoader(classLoader);
 			PrinterJob job = PrinterJob.getPrinterJob();
 			pageFormat = job.defaultPage();
-			
+
 	 }
-				
+
 	 /**
      *  Required by PluginInterface; called automatically at runtime
      *
@@ -90,12 +90,12 @@ public class PrinterPlugin implements       PrinterInterface,
     {
 				this.instanceOfMorpho = morpho;
 				initializeActions();
-        try 
+        try
         {
           ServiceController services = ServiceController.getInstance();
           services.addService(PrinterInterface.class, this);
           Log.debug(50, "Service added: PrinterInterface.");
-        } 
+        }
         catch (ServiceExistsException see)
         {
           Log.debug(1, "PrinterService registration failed: PrinterInterface");
@@ -104,108 +104,108 @@ public class PrinterPlugin implements       PrinterInterface,
 		}
 
 
-		private void initializeActions() 
-		{		
-				
+		private void initializeActions()
+		{
+
 				UIController controller = UIController.getInstance();
-				
+
 				// Action for Page setup
 				GUIAction pageSetupAction = new GUIAction("Page setup...",
 							null, new PageSetupCommand());
-				pageSetupAction.setMenuItemPosition(5);
+				pageSetupAction.setMenuItemPosition(6);
 				pageSetupAction.setSeparatorPosition(SEPARATOR_PRECEDING);
 				pageSetupAction.setMenu("File", 0);
 				pageSetupAction.setEnabled(false);  //default
 				controller.addGuiAction(pageSetupAction);
 				pageSetupAction.setEnabledOnStateChange(
-                            StateChangeEvent.CREATE_DATAPACKAGE_FRAME, 
+                            StateChangeEvent.CREATE_DATAPACKAGE_FRAME,
                             true, GUIAction.EVENT_LOCAL);
 				pageSetupAction.setEnabledOnStateChange(
-                            StateChangeEvent.CREATE_SEARCH_RESULT_FRAME, 
+                            StateChangeEvent.CREATE_SEARCH_RESULT_FRAME,
                             false, GUIAction.EVENT_LOCAL);
-														
+
 				// Action for preview
 				GUIAction previewAction = new GUIAction("Print preview...",
 							null, new PreviewCommand(this.instanceOfMorpho, this));
-				previewAction.setMenuItemPosition(6);
+				previewAction.setMenuItemPosition(7);
 				previewAction.setMenu("File", 0);
 				previewAction.setEnabled(false);  //default
 				controller.addGuiAction(previewAction);
 				previewAction.setEnabledOnStateChange(
-                            StateChangeEvent.CREATE_DATAPACKAGE_FRAME, 
+                            StateChangeEvent.CREATE_DATAPACKAGE_FRAME,
                             true, GUIAction.EVENT_LOCAL);
 				previewAction.setEnabledOnStateChange(
-                            StateChangeEvent.CREATE_SEARCH_RESULT_FRAME, 
+                            StateChangeEvent.CREATE_SEARCH_RESULT_FRAME,
                             false, GUIAction.EVENT_LOCAL);
-				
+
 				// Action for Print
 				GUIAction printAction = new GUIAction("Print...",
 							null, new PrintCommand(this.instanceOfMorpho, this));
-				printAction.setMenuItemPosition(7);
-				
+				printAction.setMenuItemPosition(8);
+
 				printAction.setMenu("File", 0);
 				printAction.setEnabled(false);  //default
 				controller.addGuiAction(printAction);
 				printAction.setEnabledOnStateChange(
-                            StateChangeEvent.CREATE_DATAPACKAGE_FRAME, 
+                            StateChangeEvent.CREATE_DATAPACKAGE_FRAME,
                             true, GUIAction.EVENT_LOCAL);
 				printAction.setEnabledOnStateChange(
-                            StateChangeEvent.CREATE_SEARCH_RESULT_FRAME, 
+                            StateChangeEvent.CREATE_SEARCH_RESULT_FRAME,
                             false, GUIAction.EVENT_LOCAL);
-														
-				
-				
-				
+
+
+
+
 		}
-		
+
 		private static String stripHTMLMetaTags(String html)
 		{
-			
+
 			int META_END = 0;
 			final int META_START = html.indexOf("<META");
-			
+
 			if (META_START>=0)  {
 				final char[] htmlChars = html.toCharArray();
 				int charIndex = META_START;
 				char nextChar = ' ';
-			
+
 				do {
 					nextChar = htmlChars[charIndex];
 					htmlChars[charIndex] = ' ';
 					charIndex++;
 				} while ((nextChar!='>') && (charIndex < htmlChars.length));
-				
+
 				html = String.valueOf(htmlChars);
 				return stripHTMLMetaTags(html);
-				
+
 			}
 			return html;
-			
+
 		}
-		
-		private static String stripComments(String html) 
+
+		private static String stripComments(String html)
 		{
-			
+
 			int prev = 0;
-			String res = ""; 
+			String res = "";
 			int pos = html.indexOf("<!--");
 			if(pos < 0) return html;
-			
+
 			while(pos>=0) {
-				 
+
 				res += html.substring(prev, pos);
 				html = html.substring(pos);
 				int next = html.indexOf("-->");
 				prev = 0;
 				html = html.substring(next+3);
 				pos = html.indexOf("<!--");
-				
+
 			}
 			res += html;
-			
+
 			return res;
 		}
-		
+
 		private String addTitleTag(String html)
 		{
 			int pos;
@@ -225,11 +225,11 @@ public class PrinterPlugin implements       PrinterInterface,
 			}
 			return init;
 		}
-		
-		
-		public void display( String displayString, String contentType) 
+
+
+		public void display( String displayString, String contentType)
 		{
-			
+
 			String ctype;
 			if(displayString == null || displayString.trim().equals("")) {
 				return;
@@ -243,11 +243,11 @@ public class PrinterPlugin implements       PrinterInterface,
 			PrintFrame frame = createFrame(displayString, ctype, PRINT_FRAME_DIMENSION);
 			frame.setPageFormat(pageFormat);
 		}
-		
-		
-		public void print( String displayString, String contentType) 
+
+
+		public void print( String displayString, String contentType)
 		{
-			
+
 			String ctype;
 			if(displayString == null || displayString.trim().equals("")) {
 				return;
@@ -262,46 +262,46 @@ public class PrinterPlugin implements       PrinterInterface,
 			frame.setPageFormat(pageFormat);
 			frame.print();
 			frame.dispose();
-			
+
 		}
-		
+
 		private String processHTMLString(String displayString) {
 			displayString = stripHTMLMetaTags(displayString);
 			displayString = stripComments(displayString);
 			displayString = addTitleTag(displayString);
 			return displayString;
 		}
-		
+
 		private PrintFrame createFrame(String displayString, String ctype, Dimension dims) {
-			
+
 			return new PrintFrame(displayString, ctype, dims);
 		}
-		
-		
+
+
 		public static void setPageFormat(PageFormat pf) {
-			
+
 			pageFormat = pf;
 		}
-		
+
 		public static PageFormat getPageFormat() {
 			return pageFormat;
 		}
 }
 
 class PageSetupCommand implements Command {
-	
+
 	PageSetupCommand() {
-		
+
 	}
-	
+
 	public void execute(ActionEvent event) {
-		
+
 		PageFormat format = PrinterPlugin.getPageFormat();
 		PrinterJob job = PrinterJob.getPrinterJob();
 		format = job.pageDialog(format);
 		PrinterPlugin.setPageFormat(format);
-		
+
 	}
 }
 
-	
+
