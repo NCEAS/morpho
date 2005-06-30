d305c7340756afc052ede2ce6405b4e1a09b9fe9
Saurabh Garg <garg@nceas.ucsb.edu>
2005-06-30 16:18:15 +0000

    New method getXSLTStylesheetLocation added to the XSLT resolver interface

--- src/edu/ucsb/nceas/morpho/plugins/XSLTResolverInterface.java
+++ src/edu/ucsb/nceas/morpho/plugins/XSLTResolverInterface.java
@@ -5,9 +5,9 @@
  *    Authors: @authors@
  *    Release: @release@
  *
- *   '$Author: sambasiv $'
- *     '$Date: 2004-04-05 21:58:20 $'
- * '$Revision: 1.2 $'
+ *   '$Author: sgarg $'
+ *     '$Date: 2005-06-30 16:18:15 $'
+ * '$Revision: 1.3 $'
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,29 +31,49 @@ import java.io.Reader;
 import edu.ucsb.nceas.morpho.util.DocumentNotFoundException;
 
 /**
- *  This interface enables access to XSLT stylesheets that are identified by 
- *  unique String DOCTYPE identifiers.  
+ *  This interface enables access to XSLT stylesheets that are identified by
+ *  unique String DOCTYPE identifiers.
  */
 public interface XSLTResolverInterface
 {
     /**
-     *  method to return a Reader object, which will provide access to a 
-     *  character-based XSLT stylesheet. The stylesheet to be returned is 
-     *  determined based on the unique DOCID String identifier passed to this 
-     *  method.  If a stylesheet corresponding to the DOCID cannot be found, 
-     *  a default or generic stylesheet may be returned.  
-     *  If no suitable stylesheet can be returned, a DocumentNotFoundException 
+     *  method to return a Reader object, which will provide access to a
+     *  character-based XSLT stylesheet. The stylesheet to be returned is
+     *  determined based on the unique DOCID String identifier passed to this
+     *  method.  If a stylesheet corresponding to the DOCID cannot be found,
+     *  a default or generic stylesheet may be returned.
+     *  If no suitable stylesheet can be returned, a DocumentNotFoundException
      *  is thrown
      *
-     *  @param docID  a unique DOCID used to determine what stylesheet to return 
+     *  @param docID  a unique DOCID used to determine what stylesheet to return
      *
-     *  @return       a Reader for the character-based XSLT stylesheet. If a 
-     *                stylesheet corresponding to the DOCID cannot be found, 
-     *                a default or generic stylesheet may be returned. 
+     *  @return       a Reader for the character-based XSLT stylesheet. If a
+     *                stylesheet corresponding to the DOCID cannot be found,
+     *                a default or generic stylesheet may be returned.
      *
      *  @throws DocumentNotFoundException if no suitable stylesheet is available
      */
-    public Reader getXSLTStylesheetReader(String docID) 
+    public Reader getXSLTStylesheetReader(String docID)
                                               throws DocumentNotFoundException;
+
+    /**
+     *  method to return a String, which will contain the name of the dir
+     *  which conatins the XSLT stylesheets. The dir to be returned is
+     *  determined based on the unique DOCID String identifier passed to this
+     *  method.  If a stylesheet corresponding to the DOCID cannot be found,
+     *  a default or generic stylesheet may be returned.
+     *
+     *  @param identifier - unique identifier used to determine the stylesheet
+     *                to return (e.g. DOCTYPE for DTD-defined XML, or
+     *                schemaLocation or rootnode namespace for XSD-defined XML)
+     *
+     *  @return       a String, which will contain the name of the dir
+     *                which conatins the XSLT stylesheets. If a
+     *                stylesheet corresponding to the DOCID cannot be found,
+     *                a default or generic stylesheet may be returned.
+     *
+     */
+    public String getXSLTStylesheetLocation(String identifier);
+
 }
 
