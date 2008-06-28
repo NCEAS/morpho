973a86d6ff0acd1fd3b4937848638ca67d1f5d3d
Jing Tao <tao@nceas.ucsb.edu>
2008-06-28 01:40:06 +0000

    Add code to handle all data directory of morpho rather than a profile.

--- src/edu/ucsb/nceas/morpho/framework/CorrectEML201DocsFrame.java
+++ src/edu/ucsb/nceas/morpho/framework/CorrectEML201DocsFrame.java
@@ -6,8 +6,8 @@
  *    Release: @release@
  *
  *   '$Author: tao $'
- *     '$Date: 2008-06-27 23:47:26 $'
- * '$Revision: 1.4 $'
+ *     '$Date: 2008-06-28 01:40:06 $'
+ * '$Revision: 1.5 $'
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -54,15 +54,19 @@ import edu.ucsb.nceas.morpho.util.Log;
  * GUI Window notifies users that morpho is correcting invalid eml 201 documents while
  * it is correcting documents. The details of invalid eml 201 documents is on: 
  * http://bugzilla.ecoinformatics.org/show_bug.cgi?id=3239
+ * This class will go through every existed profile in morpho data directory and 
+ * correct every documents in it. This class will be called in main method of Morpho class.
  * @author tao
  *
  */
 public class CorrectEML201DocsFrame extends JFrame 
 {
 	private Morpho morpho = null;
-	private boolean correctionWasDone = false;
 	private static final String TITLE = "Correcting InValid EML 2.0.1 Document";
-	private boolean hasCorrectionPath = false;
+	private static final String PROFILENAME = "profilename";
+	private static ConfigXML orginalProfile = null;
+	private Vector profilesStatusVector = new Vector();
+	//private boolean hasCorrectionPath = false;
 	
 	
 	
@@ -73,7 +77,77 @@ public class CorrectEML201DocsFrame extends JFrame
 	public CorrectEML201DocsFrame(Morpho morpho)
 	{
 		this.morpho = morpho;
+		if (morpho != null)
+		{
+			orginalProfile = morpho.getProfile();
+		}
 	    
+	}
+	
+	
+	/**
+	 * Do the correction job for given doc list.
+	 */
+	public void doCorrection()
+	{
+		if (morpho != null)
+		{
+			ConfigXML profile =morpho.getProfile();
+			boolean correctionNeeded = getCorrectionNeededStatusForMorpho();
+			if (correctionNeeded)
+			{
+				  loadGUI();
+			      //clean the Cache
+			      morpho.cleanCache();
+			      for (int i=0; i<profilesStatusVector.size(); i++)
+			      {
+			    	   ProfileCorrectionStatus correctionStatus = (ProfileCorrectionStatus)profilesStatusVector.elementAt(i);
+				       doCorrectionInOneProfile(correctionStatus);
+		               //Dipose the frame
+						if (this != null)
+						{
+							this.dispose();
+						}
+			      }
+			}
+			//set back to orginal profile.
+			morpho.setProfileDontLogin(orginalProfile);
+		}
+	}
+    
+	/*
+	 * Go through docid list from one profile and do the correction
+	 */
+	private void doCorrectionInOneProfile(ProfileCorrectionStatus correctionStatus)
+	{   
+		   if (correctionStatus != null && !correctionStatus.isEmlCorrected())
+		   {
+			   //Set morpho to a new profile
+			    morpho.setProfileDontLogin(correctionStatus.getProfile());
+		        Vector docList = getOneProfileDocList(correctionStatus.getProfile());
+		        //System.out.println("the size of doclist is "+docList.size());
+				if (docList != null && !docList.isEmpty() )
+				{
+					for (int i=0; i<docList.size(); i++)
+					{
+						String docid = null;
+						try
+						{
+					        docid = (String)docList.elementAt(i);
+					        //System.out.println("before correction docid "+docid);
+					        EML201DocumentCorrector corrector = new EML201DocumentCorrector(docid);
+					        corrector.correctDocument();
+					        Log.debug(30, "finish correcting docid(maybe skip) "+docid);
+						}
+						catch(Exception e)
+						{
+							Log.debug(30, "Couldn't correct docid "+ docid+ " since "+e.getMessage());
+						}
+					}
+	     			 // Set the "correctionWasDone" flag to true
+		     		 setCorrectionFlagTrue(correctionStatus);
+				}
+		   }
 	}
 	
 	/*
@@ -107,12 +181,43 @@ public class CorrectEML201DocsFrame extends JFrame
 	}
 	
 	/*
+	 * Goes through every profile to see if we need do correction. If one profile hasn't
+	 * been corrected, true will be returned.
+	 */
+	private boolean getCorrectionNeededStatusForMorpho()
+	{
+		boolean correctionNeeded = false;
+		if (morpho != null)
+		{
+			//Gets every profile name
+			String[] profileList = morpho.getProfilesList();
+			if (profileList != null)
+			{
+				for (int i=0; i<profileList.length; i++)
+				{
+					String profileName = profileList[i];
+					morpho.setProfileDontLogin(profileName);
+					ProfileCorrectionStatus status = getCorrectionStatusForProfile(morpho.getProfile());
+					profilesStatusVector.add(status);
+					if (status.emlCorrected == false)
+					{
+						correctionNeeded = true;
+					}
+				}
+			}
+		}
+		return correctionNeeded;
+	}
+	
+	/*
 	 * Get the flag from configuration file. This flag indicates if the local eml 201 documents
 	 * have been corrected or not.
 	 */
-	private boolean getCorrectionFlag(ConfigXML profile)
+	private ProfileCorrectionStatus getCorrectionStatusForProfile(ConfigXML profile)
 	{
+		ProfileCorrectionStatus profileCorrectionStatus= null;
 		boolean flag = true;
+		boolean hasCorrectionPath = false;
 		if(profile != null)
 		{
 			String wasCorrected = profile.get(ProfileDialog.CORRECTIONEMLPROFILEPATH, 0);
@@ -123,31 +228,41 @@ public class CorrectEML201DocsFrame extends JFrame
 			}
 			else
 			{
-				// There is no correction path in the profile.
+				// There is no correction path in the profile. 
 				hasCorrectionPath = false;
+				//So the eml documents haven't been corrected.
 				flag = false;
 			}
+			String name = profile.get(PROFILENAME, 0);
+			System.out.println("The profile name is "+ name);
+			System.out.println("Is eml corrected "+ flag);
+			System.out.println("Does the profile has the eml201corrected path "+hasCorrectionPath);
+			profileCorrectionStatus = new ProfileCorrectionStatus(profile, flag, hasCorrectionPath);
 		}
-		return flag;
+		return profileCorrectionStatus;
 	}
 	
 	/*
 	 * Set the flag to true when the correction is done.
 	 */
-	private void setCorrectionFlagTrue(ConfigXML profile)
+	private void setCorrectionFlagTrue(ProfileCorrectionStatus correctionStatus)
 	{
-		if (profile != null)
+		if (correctionStatus != null)
 		{
-			if (hasCorrectionPath)
+			ConfigXML profile = correctionStatus.getProfile();
+			if (profile != null)
 			{
-				//need to remove the file
-	            profile.removeNode(ProfileDialog.CORRECTIONEMLPROFILEPATH, 0);
-	            
+				if (correctionStatus.hasCorrectionPath())
+				{
+					//need to remove the path from the file first
+		            profile.removeNode(ProfileDialog.CORRECTIONEMLPROFILEPATH, 0);
+		            
+				}
+		         //append the correction path and value to the end.
+				profile.insert(ProfileDialog.CORRECTIONEMLPROFILEPATH, "true");
+				profile.save();
 			}
-	         //append the correction path and value to the end.
-			profile.insert(ProfileDialog.CORRECTIONEMLPROFILEPATH, "true");
-			profile.save();
-		}
+	   }
 	}
 	
 	/*
@@ -155,21 +270,24 @@ public class CorrectEML201DocsFrame extends JFrame
 	 * If nothing be found, emply vector will be returned.
 	 */
 	private Vector getOneProfileDocList(ConfigXML profile)
-	{
+	{		
 		Vector list = new Vector();
-	    String currentProfile = profile.get("profilename", 0);
-	    String separator = profile.get("separator", 0);
-	    ConfigXML config = morpho.getConfiguration();
-	    String profileDir = config.getConfigDirectory() + File.separator +
-	                       config.get("profile_directory", 0) + File.separator +
-	                       currentProfile;
-	    String datadir = profileDir + File.separator + profile.get("datadir", 0);
-	    datadir = datadir.trim();
-	    //System.out.println("the data dir is "+datadir);
-	    File xmldir = new File(datadir);
-	    
-	    // get a list of all docids	    
-	    getDocIds(xmldir, list, separator );
+		if (profile != null)
+		{
+		    String currentProfile = profile.get("profilename", 0);
+		    String separator = profile.get("separator", 0);
+		    ConfigXML config = morpho.getConfiguration();
+		    String profileDir = config.getConfigDirectory() + File.separator +
+		                       config.get("profile_directory", 0) + File.separator +
+		                       currentProfile;
+		    String datadir = profileDir + File.separator + profile.get("datadir", 0);
+		    datadir = datadir.trim();
+		    //System.out.println("the data dir is "+datadir);
+		    File xmldir = new File(datadir);
+		    
+		    // get a list of all docids	    
+		    getDocIds(xmldir, list, separator );
+		}
 		return list;
 	}
 	
@@ -232,60 +350,56 @@ public class CorrectEML201DocsFrame extends JFrame
 	        }
 
 	   }
-	
-	/**
-	 * Do the correction job for given doc list.
-	 */
-	public void doCorrection()
-	{
-		ConfigXML profile =morpho.getProfile();
-		this.correctionWasDone = getCorrectionFlag(profile);
-		if (!correctionWasDone)
-		{
-			  loadGUI();
-		      //clean the Cache
-		      morpho.cleanCache();
-		      doCorrectionInOneProfile(profile);
-               //Dipose the frame
-				if (this != null)
-				{
-					this.dispose();
-				}
-		}
-	}
-    
-	/*
-	 * Go through docid list from one profile and do the correction
-	 */
-	private void doCorrectionInOneProfile(ConfigXML profile)
-	{   
-		   if (profile != null)
-		   {
-		        Vector docList = getOneProfileDocList(profile);
-		        //System.out.println("the size of doclist is "+docList.size());
-				if (docList != null && !docList.isEmpty() )
-				{
-					for (int i=0; i<docList.size(); i++)
-					{
-						String docid = null;
-						try
-						{
-					        docid = (String)docList.elementAt(i);
-					        //System.out.println("before correction docid "+docid);
-					        EML201DocumentCorrector corrector = new EML201DocumentCorrector(docid);
-					        corrector.correctDocument();
-					        Log.debug(30, "finish correcting docid(maybe skip) "+docid);
-						}
-						catch(Exception e)
-						{
-							Log.debug(30, "Couldn't correct docid "+ docid+ " since "+e.getMessage());
-						}
-					}
-	     			 // Set the "correctionWasDone" flag to true
-		     		 setCorrectionFlagTrue(profile);
-				}
-		   }
-	}
+	  
+	   /*
+	    * This class reprents a EML201DocumentCorrection status for a profile.
+	    */
+	  class ProfileCorrectionStatus
+	  {
+		     private ConfigXML profile = null;
+		     private boolean emlCorrected = false;
+		     private boolean hasCorrectionPath = false;
+		     /**
+		      * Constructor
+		      * @param profile   this profile itself
+		      * @param emlCorrected   if eml documents were corrected in this profile
+		      * @param hasCorrectionPath  if the profile already has the new property eml201corrected in profile.xml
+		      */
+		    public ProfileCorrectionStatus(ConfigXML profile, boolean emlCorrected, boolean hasCorrectionPath )
+		    {
+		    	this.profile = profile;
+		    	this.emlCorrected = emlCorrected;
+		    	this.hasCorrectionPath = hasCorrectionPath;
+		    }
+		    
+		    /**
+		     *  Gets the profile 
+		     * @return
+		     */
+		    public ConfigXML getProfile()
+		    {
+		    	return this.profile;
+		    }
+		    
+		    /**
+		     *  Gets the status if eml documents were corrected in this profile
+		     * @return
+		     */
+		    public boolean isEmlCorrected()
+		    {
+		    	return this.emlCorrected;
+		    }
+		    
+		    /**
+		     * Gets the status if profile.xml has the new path eml201corrected
+		     * @return
+		     */
+		    public boolean hasCorrectionPath()
+		    {
+		    	return this.hasCorrectionPath;
+		    }
+		    
+	  }
 
 
 }
