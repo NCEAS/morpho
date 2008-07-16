bef8b1582037e6d11b5060f2d9fc2cf160f9ad09
Jing Tao <tao@nceas.ucsb.edu>
2008-07-16 21:05:10 +0000

    An exception class to handle local file existing.

---
+++ src/edu/ucsb/nceas/morpho/datastore/LocalFileExistingException.java
@@ -0,0 +1,38 @@
+package edu.ucsb.nceas.morpho.datastore;
+/**
+ *  '$RCSfile: LocalFileExistingException.java,v $'
+ *  Copyright: 2000 Regents of the University of California and the
+ *              National Center for Ecological Analysis and Synthesis
+ *    Authors: @authors@
+ *    Release: @release@
+ *
+ *   '$Author: tao $'
+ *     '$Date: 2008-07-16 21:05:10 $'
+ * '$Revision: 1.1 $'
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/**
+ * An exception class to handle local file already exists.
+ */
+public class LocalFileExistingException extends Exception 
+{
+     public LocalFileExistingException(String message)
+     {
+    	 super(message);
+     }
+
+}
