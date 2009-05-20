1947604ead3d1a831d54b09f5f3582b29d6b4792
Jing Tao <tao@nceas.ucsb.edu>
2009-05-20 21:07:14 +0000

    Change to 1.7.0-RC3

--- src/edu/ucsb/nceas/morpho/Morpho.java
+++ src/edu/ucsb/nceas/morpho/Morpho.java
@@ -6,8 +6,8 @@
  *    Release: @release@
  *
  *   '$Author: tao $'
- *     '$Date: 2009-05-13 01:09:15 $'
- * '$Revision: 1.103 $'
+ *     '$Date: 2009-05-20 21:07:14 $'
+ * '$Revision: 1.104 $'
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -118,7 +118,7 @@ import com.ice.tar.TarEntry;
 public class Morpho
 {
     /** The version of this release of Morpho */
-    public static String VERSION = "1.7.0-RC2";
+    public static String VERSION = "1.7.0-RC3";
 
     /** Constant to indicate a separator should precede an action */
     public static String SEPARATOR_PRECEDING = "separator_preceding";
