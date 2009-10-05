d78cd715fbf06b642284c3d87baf5727d59c17c7
Jing Tao <tao@nceas.ucsb.edu>
2009-10-05 18:37:43 +0000

    add a new class to hold the wizard page name and parameters.

---
+++ src/edu/ucsb/nceas/morpho/util/WizardPageInfo.java
@@ -0,0 +1,74 @@
+/**
+ *  '$RCSfile: Util.java,v $'
+ *  Copyright: 2000 Regents of the University of California and the
+ *              National Center for Ecological Analysis and Synthesis
+ *    Authors: @authors@
+ *    Release: @release@
+ *
+ *   '$Author: tao $'
+ *     '$Date: 2008-11-11 23:51:05 $'
+ * '$Revision: 1.2 $'
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package edu.ucsb.nceas.morpho.util;
+
+/**
+ * It represents a wizard page information in incomplete-document medata part.
+ * It include the wizard page class name and an array of parameter (string)
+ * @author tao
+ *
+ */
+public class WizardPageInfo 
+{
+    private String className = null;
+    private String[] parameters = null;
+    
+    /**
+     * Constructor for given className
+     * @param className
+     */
+    public WizardPageInfo(String className)
+    {
+    	this.className = className;   	
+    }
+
+    /**
+     * Get class name of the page
+     * @return
+     */
+	public String getClassName() 
+	{
+		return className;
+	}
+
+	/**
+	 * Gets the parameter list of the class
+	 * @return
+	 */
+	public String[] getParameters() 
+	{
+		return parameters;
+	}
+
+	/**
+	 * Sets the parameter list of the class
+	 * @param parameters
+	 */
+	public void setParameters(String[] parameters) 
+	{
+		this.parameters = parameters;
+	}
+}
