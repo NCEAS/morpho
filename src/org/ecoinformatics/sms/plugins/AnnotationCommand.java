33a10ff61ba51fc639425e95208fbf22dd3bd72d
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2009-11-12 00:42:01 +0000

    annotation editing page and the command to display/modify attribute-level annotations. commented out SMS calls so that the jar dependencies don't creep up on other Morpho developers.

--- src/org/ecoinformatics/sms/plugins/AnnotationCommand.java
+++ src/org/ecoinformatics/sms/plugins/AnnotationCommand.java
@@ -27,16 +27,34 @@
 package org.ecoinformatics.sms.plugins;
 
 import java.awt.event.ActionEvent;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JTable;
 
+import org.ecoinformatics.sms.SMS;
+import org.ecoinformatics.sms.annotation.Annotation;
+import org.ecoinformatics.sms.annotation.Mapping;
+import org.ecoinformatics.sms.annotation.Measurement;
+import org.ecoinformatics.sms.annotation.Observation;
+
+import edu.ucsb.nceas.morpho.Morpho;
 import edu.ucsb.nceas.morpho.datapackage.AbstractDataPackage;
+import edu.ucsb.nceas.morpho.datapackage.AccessionNumber;
 import edu.ucsb.nceas.morpho.datapackage.DataViewContainerPanel;
 import edu.ucsb.nceas.morpho.datapackage.DataViewer;
+import edu.ucsb.nceas.morpho.datastore.FileSystemDataStore;
+import edu.ucsb.nceas.morpho.framework.ModalDialog;
 import edu.ucsb.nceas.morpho.framework.MorphoFrame;
 import edu.ucsb.nceas.morpho.framework.UIController;
 import edu.ucsb.nceas.morpho.util.Command;
 import edu.ucsb.nceas.morpho.util.Log;
+import edu.ucsb.nceas.morpho.util.UISettings;
 
 /**
  * Class to handle edit column meta data command
@@ -47,12 +65,14 @@ public class AnnotationCommand implements Command {
 
 	private AbstractDataPackage adp = null;
 	private DataViewer dataView = null;
-	private int attributeIndex = -1;
-	private int entityIndex = -1;
 	private JTable table = null;
 	private DataViewContainerPanel resultPane = null;
+	private int entityIndex = -1;
+	private int attributeIndex = -1;
 	private String entityName;
 	private String attributeName;
+	private AnnotationPage annotationPage = new AnnotationPage();
+	private Annotation annotation = null;
 
 	/**
 	 * Constructor
@@ -95,11 +115,83 @@ public class AnnotationCommand implements Command {
 				entityName = adp.getEntityName(entityIndex);
 				attributeName = adp.getAttributeName(entityIndex, attributeIndex);
 				
-				Log.debug(5, "Annotating entity: " + entityName + ", attribute: " + attributeName);
+				Log.debug(30, "Annotating entity: " + entityName + ", attribute: " + attributeName);
+				
+				// look up the annotation if it exists, or make new one
+				String packageId = adp.getAccessionNumber();
+				String dataTable = String.valueOf(entityIndex);
+				
+				//List<Annotation> annotations = SMS.getInstance().getAnnotationManager().getAnnotations(packageId, dataTable);
+				List<Annotation> annotations = new ArrayList<Annotation>();
+
+				if (annotations.size() > 0) {
+					annotation = annotations.get(0);
+				} else {
+					// create a new one
+					annotation = new Annotation();
+					annotation.setEMLPackage(packageId);
+					annotation.setDataTable(dataTable);
+				}
+				
+				if (showDialog()) {
+					
+					//set up the attribute->measurement mapping only 1:1 now
+					Mapping mapping = new Mapping();
+					mapping.setAttribute(attributeName);
+					Observation observation = annotationPage.getObservation();
+					Measurement measurement = observation.getMeasurements().get(0);
+					mapping.setMeasurement(measurement);
+					annotation.addObservation(observation);
+					annotation.addMapping(mapping);
+					
+					//about to save
+					AccessionNumber accNum = new AccessionNumber(Morpho.thisStaticInstance);
+					annotation.setURI(accNum.getNextId());
+					
+					saveAnnotation();
+					
+				}
 			}
 
 		}
-
+	}
+	
+	private boolean showDialog() {
+		annotationPage = new AnnotationPage();
+		annotationPage.setAnnotation(annotation);
+		
+		// show the dialog
+		ModalDialog dialog = 
+			new ModalDialog(
+					annotationPage,
+					UIController.getInstance().getCurrentActiveWindow(),
+					UISettings.POPUPDIALOG_WIDTH,
+					UISettings.POPUPDIALOG_HEIGHT);
+		
+		//get the response back
+		return (dialog.USER_RESPONSE == ModalDialog.OK_OPTION);
+	}
+	
+	private void saveAnnotation() {
+		
+		try {
+			FileSystemDataStore fds = new FileSystemDataStore(Morpho.thisStaticInstance);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			annotation.write(baos);
+			
+			//save in local store
+			File annotationFile = fds.saveFile(annotation.getURI(), new StringReader(baos.toString()));
+			
+			//save in the manager - probably don't need this since it's all in memory and referencing the same annotation object
+//			if (SMS.getInstance().getAnnotationManager().isAnnotation(annotation.getURI())) {
+//				SMS.getInstance().getAnnotationManager().updateAnnotation(new FileInputStream(annotationFile), annotation.getURI());
+//			} else {
+//				SMS.getInstance().getAnnotationManager().importAnnotation(new FileInputStream(annotationFile), annotation.getURI());
+//			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
