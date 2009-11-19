0444b3b004416f3f3345f0aed884781b4b2a4c5c
Jing Tao <tao@nceas.ucsb.edu>
2009-11-19 19:07:07 +0000

    Impletes the setPageData for simple delimiter.

--- src/edu/ucsb/nceas/morpho/plugins/datapackagewizard/pages/DataFormat.java
+++ src/edu/ucsb/nceas/morpho/plugins/datapackagewizard/pages/DataFormat.java
@@ -41,6 +41,7 @@ import edu.ucsb.nceas.morpho.plugins.datapackagewizard.WizardContainerFrame;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
@@ -92,13 +93,16 @@ public class DataFormat extends AbstractUIPage{
   private final String RASTER_XPATH
           = "/eml:eml/dataset/dataTable/physical/dataFormat/binaryRasterFormat";
 
-  private JPanel radioPanel;
-  private JPanel simpleTextpanel;
-  private JPanel complexTextPanel;
-  private JPanel proprietaryPanel;
-  private JPanel rasterPanel;
-  private JPanel currentPanel;
-  private JLabel radioButtonGrpLabel;
+  private JPanel radioPanel = null;
+  private JPanel simpleTextpanel = null;;
+  private JPanel complexTextPanel = null;
+  private JPanel proprietaryPanel = null;
+  private JPanel rasterPanel = null;
+  private JPanel currentPanel = null;
+  private JPanel orientationSimpleTextPanel = null;
+  private JPanel orientationComplexTextPanel = null;
+  private JPanel simpleDelimiterCheckBoxPanel = null;
+  private JLabel radioButtonGrpLabel = null;
   private final String[] buttonsText = new String[] {
     "Simple delimited text format (uses one or more delimiters throughout the "
     + "file).",
@@ -134,7 +138,17 @@ public class DataFormat extends AbstractUIPage{
   private CustomList list;
   private WizardContainerFrame mainWizFrame;
   private boolean fromCorrectionWizard = false;
-  
+  private static final int SIMPLETEXTCHOICE = 0;
+  private static final int COMPLEXTEXTCHOICE =1;
+  private static final int PROPRIETARYCHOICE = 2;
+  private static final int COLUMNORIENTATIONCHOICE = 0;
+  private static final int ROWORIENTATIONCHOICE = 1;
+  private static final int TABCHOICE = 0;
+  private static final int COMMACHOICE = 1;
+  private static final int SPACECHOICE = 2;
+  private static final int SEMICOLONCHOICE = 3;
+  private static final int OTHERCHOICE= 4;
+ 
  // private String fileName = "";
 
   // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -259,8 +273,10 @@ public class DataFormat extends AbstractUIPage{
     JLabel spacerLabel = WidgetFactory.makeLabel(EMPTY_STRING, false);
 
     orientationPanel.add(spacerLabel);
-
-    orientationPanel.add(getOrientationRadioPanel());
+    
+    orientationSimpleTextPanel = getOrientationRadioPanel();
+    
+    orientationPanel.add(orientationSimpleTextPanel);
 
     panel.add(orientationPanel);
 
@@ -307,7 +323,9 @@ public class DataFormat extends AbstractUIPage{
 
     orientationPanel.add(spacerLabel);
 
-    orientationPanel.add(getOrientationRadioPanel());
+    orientationComplexTextPanel = getOrientationRadioPanel();
+    
+    orientationPanel.add(orientationComplexTextPanel);
 
     panel.add(orientationPanel);
 
@@ -501,6 +519,147 @@ public class DataFormat extends AbstractUIPage{
       otherProprietaryTextField.setEnabled(false);
       proprietaryText = ((String)(mimeMap.get(labelString.trim()))).trim();
     }
+  }
+  
+  /*
+   * When we get a delimiter from eml, we need to select check box accordingly in UI
+   */
+  private void selectSimpleDelimiterCheckBox(String delimiter) throws Exception
+  {
+	  if(delimiter != null)
+	  {
+		  if(delimiter.equals(WizardSettings.HEX_VALUE_TAB))
+		  {
+			  Log.debug(35, "click on tab box in DataFormat selectSimpleDelimiterCheckBox");
+			  selectSimpleDelimiterCheckBox(TABCHOICE);
+		  }
+		  else if (delimiter.equals(","))
+		  {
+			  Log.debug(35, "click on comma box in DataFormat selectSimpleDelimiterCheckBox");
+			  selectSimpleDelimiterCheckBox(COMMACHOICE);
+		  }
+		  else if (delimiter.equals(WizardSettings.HEX_VALUE_SPACE))
+		  {
+			  Log.debug(35, "click on space box in DataFormat selectSimpleDelimiterCheckBox");
+			  selectSimpleDelimiterCheckBox(SPACECHOICE);
+		  }
+		  else if(delimiter.equals(";"))
+		  {
+			  Log.debug(35, "click on semicolon box in DataFormat selectSimpleDelimiterCheckBox");
+			  selectSimpleDelimiterCheckBox(SEMICOLONCHOICE);
+		  }
+		  else
+		  {
+			  Log.debug(35, "click on other box in DataFormat selectSimpleDelimiterCheckBox");
+			  selectSimpleDelimiterCheckBox(OTHERCHOICE);
+			  otherDelimTextFieldSimple.setEnabled(true);
+			  otherDelimTextFieldSimple.setText(delimiter);
+		  }
+		  
+	
+			
+	  }
+  }
+  
+  /*
+   * Selects the check box base on index in simple delimiter check box panel
+   */
+  private void selectSimpleDelimiterCheckBox(int index) throws Exception
+  {
+	  if (simpleDelimiterCheckBoxPanel != null)
+	  {
+		try
+		{
+		    JCheckBox box = (JCheckBox) simpleDelimiterCheckBoxPanel.getComponent(index);
+		    box.setSelected(true);
+		}
+		catch(Exception e)
+		{
+			throw e;
+		}
+	  }
+	  else
+	  {
+		  throw new Exception("The simpleDelimiter check box is null in DataFormat.selectSimpeDelimterCheckBox");
+	  }
+  }
+  
+  /*
+   * Click radio button for first question.
+   * 0 - simple delimiter text
+   * 1 -  complex delimiter text 
+   * 2 -  non-text proprietary 
+   */
+  private void clickDataFormatRadioButton(int index) throws Exception
+  {
+	     clickRadionButton(radioPanel, index);
+  }
+  
+  /*
+   * Click the radio button for data orientation.
+   * 0 - column orientation
+   * 1 - row orientation
+   */
+  private void clickDataOrientationButtonInSimpleTextFormat(String orient) throws Exception
+  {
+	  clickDataOrientationButton(orientationSimpleTextPanel, orient);
+  }
+  
+  /*
+   * Click the radio button for data orientation.
+   * 0 - column orientation
+   * 1 - row orientation
+   */
+  private void clickDataOrientationButtonInComplexTextFormat(String orient) throws Exception
+  {
+	  clickDataOrientationButton(orientationComplexTextPanel, orient);
+  }
+  
+  /*
+   * Click the radio button base on orientation.
+   * column - click 0
+   * row - click 1
+   */
+  private void clickDataOrientationButton(JPanel container, String orientation) throws Exception
+  {
+	  if(orientation != null && orientation.equals(COLUMN_MAJOR))
+	  {
+		  clickRadionButton(container, COLUMNORIENTATIONCHOICE);
+	  }
+	  else if(orientation != null && orientation.equals(ROW_MAJOR))
+	  {
+		  clickRadionButton(container, ROWORIENTATIONCHOICE);
+	  }
+	  else
+	  {
+		  throw new Exception("Morpho couldn't understand the orienation "+orientation);
+	  }
+		   
+  }
+  
+  /*
+   * Click button in radio panel
+   */
+  private void clickRadionButton(JPanel container, int index) throws Exception
+  {
+	  if(container != null)
+	  {
+		 try
+		 {
+		     Container radio = (Container)container.getComponent(1);
+		     JRadioButton button = (JRadioButton)radio.getComponent(index);
+		     button.doClick();
+		 }
+		 catch(Exception e)
+		 {
+			 throw e;
+		 }
+	  }
+	  else
+	  {
+		 Log.debug(10, "The Radion button container is null and we couldn't click it in DataFormat.clickRadioButton");
+	     throw new Exception("The Radion button container is null and we couldn't click it in DataFormat.clickRadioButton");
+	  }
   }
 
 
@@ -864,7 +1023,7 @@ public class DataFormat extends AbstractUIPage{
 
     JPanel panel = WidgetFactory.makeVerticalPanel(7);
 
-    JPanel cbPanel = WidgetFactory.makeCheckBoxPanel(delimiterCheckBoxesText, -1,
+    simpleDelimiterCheckBoxPanel = WidgetFactory.makeCheckBoxPanel(delimiterCheckBoxesText, -1,
       new ItemListener() {
 
         public void itemStateChanged(ItemEvent e) {
@@ -903,7 +1062,7 @@ public class DataFormat extends AbstractUIPage{
           }
         }
       });
-    panel.add(cbPanel);
+    panel.add(simpleDelimiterCheckBoxPanel);
 
     otherDelimTextFieldSimple.setPreferredSize(WizardSettings.WIZARD_CONTENT_LABEL_DIMS);
     otherDelimTextFieldSimple.setMaximumSize(WizardSettings.WIZARD_CONTENT_LABEL_DIMS);
@@ -943,8 +1102,8 @@ public class DataFormat extends AbstractUIPage{
 
   private void setOrientation(String orient) {
 
-    if (formatXPath==SIMPLE_TEXT_XPATH)  orientationSimple  = orient;
-    if (formatXPath==COMPLEX_TEXT_XPATH) orientationComplex = orient;
+    if (formatXPath.equals(SIMPLE_TEXT_XPATH))  orientationSimple  = orient;
+    if (formatXPath.equals(COMPLEX_TEXT_XPATH)) orientationComplex = orient;
   }
 
 
@@ -992,17 +1151,58 @@ public class DataFormat extends AbstractUIPage{
    */
   public boolean setPageData(OrderedMap data, String xPathRoot) 
   {
+	  boolean success = false;
+	  Log.debug(35, "In DataFormat.setPageData, the xpathRoot is "+xPathRoot+" and map is "+data.toString() );
+	  if(data == null && xPathRoot == null)
+	  {
+		  Log.debug(30, "The map or xpathroot is null in DataFormat.setPageData and return false");
+		  return success;
+	  }
+	  Vector keyNeedToDelete = new Vector();
 	  formatXPath = findFormatType(data, xPathRoot);
+	  Iterator keyIt = data.keySet().iterator();
+	   String nextXPath = null;
+	   String nextVal = null;
 	  if (formatXPath.equals(SIMPLE_TEXT_XPATH))
 	  {
-		  String nextVal = (String)data.get(xPathRoot+ "/textFormat/attributeOrientation");
-		  setOrientation(nextVal);
-		  nextVal = (String)data.get(xPathRoot+ "/textFormat/attributeOrientation");
-		  return false;
+		  try
+		  {
+			  clickDataFormatRadioButton(SIMPLETEXTCHOICE);
+			  while(keyIt.hasNext())
+			  {
+				 nextXPath = (String)keyIt.next();
+			     if(nextXPath == null)
+			     {
+			    	 continue;
+			     }
+			     else if(nextXPath.equals(xPathRoot+ "/textFormat/attributeOrientation"))
+			     {
+				   nextVal = (String)data.get(nextXPath);
+				   Log.debug(35, "DataFormat.setPageData - in simple text format to set Orientaion to "+nextVal);
+			       setOrientation(nextVal);
+			       clickDataOrientationButtonInSimpleTextFormat(nextVal);
+			       
+			     }
+			     else if(nextXPath.startsWith(xPathRoot+"/textFormat/simpleDelimited/fieldDelimiter"))
+			     {
+			    	 nextVal = (String)data.get(nextXPath);
+			    	 Log.debug(35, "DataFormat.setPageData - in simple text format to get field delimiter "+nextVal);
+			    	 selectSimpleDelimiterCheckBox(nextVal);
+			     }			     
+			     keyNeedToDelete.add(nextXPath);			     
+			  }
+		  }
+		  catch(Exception e)
+		  {
+			  Log.debug(5, "Couldn't popluate metadata into DataForat page since "+e.getMessage());
+		      return success;
+		  }
+		  success = this.removeAllDataInMap(data, keyNeedToDelete);
+		  return success;
 	  }
 	  else if(formatXPath.equals(COMPLEX_TEXT_XPATH))
 	  {		
-		  String nextVal = (String)data.get(xPathRoot+ "/textFormat/attributeOrientation");
+		  nextVal = (String)data.get(xPathRoot+ "/textFormat/attributeOrientation");
 		  setOrientation(nextVal);
 		  //need to be implement
 		  return false;
@@ -1015,8 +1215,45 @@ public class DataFormat extends AbstractUIPage{
 	  {
 		  Log.debug(30, "Couldn't find the data format type with xpathroot "+xPathRoot+" in the map"+data);
 		  return false;
+	  } 
+  }
+  
+  private boolean removeAllDataInMap(OrderedMap map, Vector keyList)
+  {
+	  boolean success = false;
+	  if(map == null)
+	  {
+		  success = true;
+		  return success;
 	  }
-	  //return false; 
+	  else
+	  {
+		  if(keyList == null)
+		  {
+			  return success;
+		  }
+		  else
+		  {
+			  for(int i=0; i<keyList.size(); i++)
+			  {
+				  String key = (String)keyList.elementAt(i);
+				  if(key != null)
+				  {
+					  map.remove(key);
+				  }
+			  }
+			  success = map.isEmpty();
+		  }
+	  }
+	  if(success)
+	  {
+		  Log.debug(35, "Map in DataFormat.setPageData was removed completely!");
+	  }
+	  else
+	  {
+		  Log.debug(35, "Map in DataFormat.setPageData couldn't be removed completely. It still has "+map.toString());
+	  }
+	  return success;
   }
   
   /*
