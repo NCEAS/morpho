ec8f828721d375824752685fab4b3ccb5d4a6a4d
Jing Tao <tao@nceas.ucsb.edu>
2009-11-20 21:29:01 +0000

    Add a method which can extract a sub-map from given map and path.

--- src/edu/ucsb/nceas/morpho/util/Util.java
+++ src/edu/ucsb/nceas/morpho/util/Util.java
@@ -27,6 +27,7 @@ package edu.ucsb.nceas.morpho.util;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
 
 import org.w3c.dom.Node;
 
@@ -43,6 +44,9 @@ import edu.ucsb.nceas.utilities.XMLUtilities;
  */
 public class Util 
 {
+	    private static final String LEFTBRACKET = "[";
+	    private static final String RIGHTBRACKET = "]";
+	    private static final String SLASH = "/";
 		/**
 		 * Determine if the specified string is blank or not. If string is null, it will be false.
 		 * @param input the specified string
@@ -128,6 +132,138 @@ public class Util
 		      }
 		      return existingValuesMap;
 	}
+		
+    /**
+     * This is the method will extract sub-map from original map. (originalMap will be removed those elements too).
+     * For example if the original map looks like:
+     * /attributeList/attribute[1]/@id	 = 	1258682693040
+     * /attributeList/attribute[1]/attributeName[1]	 = 	1
+     * /attributeList/attribute[1]/attributeDefinition[1]	 = 	1
+     * /attributeList/attribute[1]/measurementScale[1]/dateTime[1]/formatString[1]	 = 	1
+     * /attributeList/attribute[2]/@id	 = 	1258682693041
+     * /attributeList/attribute[2]/attributeName[1]	 = 	2
+     * /attributeList/attribute[2]/attributeDefinition[1]	 = 	2
+     * /attributeList/attribute[2]/measurementScale[1]/dateTime[1]/formatString[1]	 = 	2
+     * and path like /attributeList/attribute.
+     * The result will be:
+     * /@id	 = 	1258682693040
+     * /attributeName[1]	 = 	1
+     * /attributeDefinition[1]	 = 	1
+     * /measurementScale[1]/dateTime[1]/formatString[1]	 = 	1
+     * @param originalMap
+     * @param path
+     * @return
+     */
+    public static OrderedMap getFirstPartialMapForPath(OrderedMap originalMap, String path)
+    {
+    	OrderedMap newMap = new OrderedMap();   	
+    	if(originalMap != null && path != null)
+    	{
+    		String newPath = path;
+    		String newKey = null;
+    		Vector deleteKey = new Vector();
+    		String count = null;
+    		boolean firstTime = true;
+    		Iterator it = originalMap.keySet().iterator();
+    		boolean hasPredictNumber = true;
+    		while(it.hasNext())
+    		{
+    			String key = (String)it.next();
+    			if(key == null)
+    			{
+    				continue;
+    			}
+    			Log.debug(35, "The orginal key in map is "+key+" in Util.getFirstPartialMapFroPath");
+    			int index = key.indexOf(newPath);
+    			Log.debug(35, "The index of "+newPath+" in key "+key+" is "+index);
+    			if(firstTime && index != -1)
+    			{
+    				//since path is /attributeList/attribute, we need to figure out the real path
+    				// is /attributeList/attribute[1]. So we need to know the first "/" after path
+                    int indexOfSlash = key.indexOf(SLASH, index+newPath.length()-1);
+                    //now we can set the newPath to real path
+                    if(indexOfSlash != -1)
+                    {
+                      newPath = key.substring(0, indexOfSlash);
+                      newKey = key.substring(indexOfSlash);
+                    }
+                    else
+                    {
+                    	//we couldn't find the slash after, so put whole key as new path.
+                    	newPath = key;
+                    	//newKey will be set to origninal key too.
+                        newKey = key;
+                    }
+                    String value = (String)originalMap.get(key);
+                    if(newKey != null && value != null)
+                    {
+                    	Log.debug(35, "Put the key "+newKey +" value "+value+" into the new map");
+                    	newMap.put(newKey, value);
+                    }                   
+                     deleteKey.add(key);
+    				 firstTime = false;
+    			}
+    			else if(index != -1)
+    			{
+    				 newKey = key.substring(index+newPath.length());
+    				 String value = (String)originalMap.get(key);
+                     if(newKey != null && value != null)
+                     {
+                     	Log.debug(35, "Put the key "+newKey +" value "+value+" into the new map");
+                     	newMap.put(newKey, value);
+                     }                   
+                    deleteKey.add(key);
+    			}
+    		}
+    		//remove the deleted key
+        	for(int j=0; j<deleteKey.size(); j++)
+        	{
+        		String delete = (String)deleteKey.elementAt(j);
+        		originalMap.remove(delete);
+        	}
+        	Log.debug(35, "The original map in Util.getFirstPartialMapForPath now is "+originalMap.toString());
+    	}
+    	Log.debug(35, "The return map in Util.getFirstPartialMapForPath now is "+newMap.toString());
+    	return newMap;
+    }
+    
+    /*
+     * Get a count string in an predict. If we couldn't find one, empty string will be returned.
+     * [4] will be return 4.
+     */
+    private static String getCountStringofPedict(String predictStr)
+    {
+    	String count = "";
+    	if(predictStr != null)
+    	{
+    		boolean open = false;
+    		boolean close = false;
+    		for(int i=0; i<predictStr.length(); i++)
+    		{
+    			String str = predictStr.substring(i, i+1);
+    			if(open && !close)
+    			{
+    			   count = count+str;
+    			}
+    			if(str.equals(LEFTBRACKET))
+    			{
+    				open = true;
+    			}
+    			else if(str.equals(RIGHTBRACKET))
+    			{
+    				close = true;
+    			} 			
+    		}
+    		//if no close, we set it ""
+    		if(!close)
+    		{
+    			Log.debug(35, "There is no close bracket, we set the count to empty");
+    			count="";
+    		}
+    	}
+    	Log.debug(30, "The count string in string "+predictStr+" is "+count+" in Util.getCountStringofPedict");
+    	return count;
+    }
 		
 	/**
 	 * Delete the auto-saved file for given abstract data package.
