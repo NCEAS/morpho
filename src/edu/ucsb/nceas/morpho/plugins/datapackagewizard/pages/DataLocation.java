aa392c652a0293e3bc9df959dd23da9f22b1666e
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2010-04-29 18:34:01 +0000

    Move FileChooserWidget to public class so that it can be reused by other UI pages http://bugzilla.ecoinformatics.org/show_bug.cgi?id=3473

--- src/edu/ucsb/nceas/morpho/plugins/datapackagewizard/pages/DataLocation.java
+++ src/edu/ucsb/nceas/morpho/plugins/datapackagewizard/pages/DataLocation.java
@@ -82,31 +82,31 @@ public class DataLocation extends AbstractUIPage {
 
   // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-  private final String FILE_LOCATOR_FIELD_FILENAME_LABEL = "File Name:";
-  private final String FILE_LOCATOR_FIELD_OBJNAME_LABEL  = "Name/Title:";
+  public static final String FILE_LOCATOR_FIELD_FILENAME_LABEL = "File Name:";
+  public static final String FILE_LOCATOR_FIELD_OBJNAME_LABEL  = "Name/Title:";
 
-  private final String INIT_FILE_LOCATOR_TEXT
+  public static final String INIT_FILE_LOCATOR_TEXT
                                   = "   use button to select a file -->";
-  private final String FILE_LOCATOR_IMPORT_DESC_INLINE
+  public static final String FILE_LOCATOR_IMPORT_DESC_INLINE
         = WizardSettings.HTML_TABLE_LABEL_OPENING
         +"Use the \"locate\" button to locate the data file on your computer:"
         +WizardSettings.HTML_TABLE_LABEL_CLOSING;
 
-  private final String URN_ROOT = "ecogrid://knb/";
+  public static final String URN_ROOT = "ecogrid://knb/";
 
-  private final String OBJECTNAME_XPATH
+  public static final String OBJECTNAME_XPATH
       = "/eml:eml/dataset/dataTable/physical/objectName";
 
-  private final String MEDIUMNAME_XPATH
+  public static final String MEDIUMNAME_XPATH
       = "/eml:eml/dataset/dataTable/physical/distribution/offline/mediumName";
 
-  private final String INLINE_XPATH
+  public static final String INLINE_XPATH
       = "/eml:eml/dataset/dataTable/physical/distribution/inline";
 
-  private final String ONLINE_URL_XPATH
+  public static final String ONLINE_URL_XPATH
       = "/eml:eml/dataset/dataTable/physical/distribution/online/url";
 
-  private final String FILECHOOSER_PANEL_TITLE = "File Location:";
+  public static final String FILECHOOSER_PANEL_TITLE = "File Location:";
 
   private final String Q1_TITLE = "What do you want to do?";
 
@@ -1773,132 +1773,3 @@ public class DataLocation extends AbstractUIPage {
 
 }
 
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
-
-class FileChooserWidget extends JPanel {
-
-  private final       String EMPTY_STRING = "";
-  private JLabel     fileNameLabel;
-  private JLabel     descLabel;
-  private JTextField fileNameField;
-  private JButton    fileNameButton;
-  private String     importFileURL;
-
-  static private File lastDataDir = null;
-
-  public FileChooserWidget(String label, String descText, String initialText) {
-
-    super();
-    init();
-    if (initialText==null) initialText = EMPTY_STRING;
-    fileNameField.setText(initialText);
-    setDescription(descText);
-    setLabelText(label);
-  }
-
-  private void init() {
-
-    this.setLayout(new GridLayout(3,1));
-
-    descLabel = WidgetFactory.makeHTMLLabel(EMPTY_STRING, 1);
-    this.add(descLabel);
-
-    ////
-    JPanel fileNamePanel = WidgetFactory.makePanel();
-    fileNamePanel.setLayout(new BorderLayout());
-    fileNamePanel.setMaximumSize(WizardSettings.WIZARD_CONTENT_SINGLE_LINE_DIMS);
-
-    fileNameLabel = WidgetFactory.makeLabel(EMPTY_STRING, true);
-
-    fileNameLabel.setBackground(Color.lightGray);
-    fileNamePanel.add(fileNameLabel, BorderLayout.WEST);
-
-    fileNameField = WidgetFactory.makeOneLineTextField();
-    fileNameField.setEnabled(false);
-    fileNameField.setEditable(false);
-    fileNameField.setDisabledTextColor(
-                                      WizardSettings.WIZARD_CONTENT_TEXT_COLOR);
-    fileNamePanel.add(fileNameField, BorderLayout.CENTER);
-
-    fileNameButton = WidgetFactory.makeJButton("locate...",
-
-            new ActionListener() {
-
-              public void actionPerformed(ActionEvent e) {
-
-                final JFileChooser fc = new JFileChooser();
-                fc.setDialogTitle("Select a data file to import...");
-                String userdir = System.getProperty("user.dir");
-                String homedir = System.getProperty("user.home");
-                String osname = System.getProperty("os.name");
-                if (lastDataDir==null) {
-                  fc.setCurrentDirectory(new File(userdir));
-                  if (osname.indexOf("Window")>-1) {  // a windows os
-                    File mydocs = new File(homedir+File.separator+"My Documents");
-                    if (mydocs.exists()) {
-                      fc.setCurrentDirectory(mydocs);
-                    }
-                  }
-                } else {  // use previous dataDirectory
-                  fc.setCurrentDirectory(lastDataDir);
-                }
-                int returnVal = fc.showOpenDialog(WizardContainerFrame.frame);
-                File file = null;
-                if (returnVal == JFileChooser.APPROVE_OPTION) {
-
-                  file = fc.getSelectedFile();
-
-                  if (file!=null) {
-                    lastDataDir = new File(file.getParent());
-                    setImportFileURL(file.getAbsolutePath());
-                    fileNameField.setText(getImportFileURL());
-                  }
-                }
-              }
-            });
-
-    fileNamePanel.add(fileNameButton, BorderLayout.EAST);
-
-    this.add(fileNamePanel);
-  }
-
-  protected JLabel  getLabel()  { return this.fileNameLabel; }
-
-  protected void setLabelText(String text) { this.fileNameLabel.setText(text); }
-
-  protected JButton getButton() { return this.fileNameButton; }
-
-  protected JTextField getTextArea() { return this.fileNameField; }
-
-  protected String  getImportFileURL() { return this.importFileURL; }
-
-  protected void setDescription(String desc) {
-
-    if (desc==null) desc = EMPTY_STRING;
-    descLabel.setText(desc);
-  }
-
-  protected void setImportFileURL(String filePath) {
-
-    this.importFileURL = filePath;
-  }
-  
-  /**
-   * Sets the chosen file name
-   * @param fileName
-   */
-  public void setChosenFileName(String fileName)
-  {
-	  if(fileName != null)
-	  {
-		  fileNameField.setText(fileName);
-		  setImportFileURL(fileName);
-	  }
-  }
-}
---
+++ src/edu/ucsb/nceas/morpho/plugins/datapackagewizard/pages/FileChooserWidget.java
@@ -0,0 +1,140 @@
+package edu.ucsb.nceas.morpho.plugins.datapackagewizard.pages;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import edu.ucsb.nceas.morpho.plugins.datapackagewizard.WidgetFactory;
+import edu.ucsb.nceas.morpho.plugins.datapackagewizard.WizardContainerFrame;
+import edu.ucsb.nceas.morpho.plugins.datapackagewizard.WizardSettings;
+
+public class FileChooserWidget extends JPanel {
+
+	  private final       String EMPTY_STRING = "";
+	  private JLabel     fileNameLabel;
+	  private JLabel     descLabel;
+	  private JTextField fileNameField;
+	  private JButton    fileNameButton;
+	  private String     importFileURL;
+
+	  static private File lastDataDir = null;
+
+	  public FileChooserWidget(String label, String descText, String initialText) {
+
+	    super();
+	    init();
+	    if (initialText==null) initialText = EMPTY_STRING;
+	    fileNameField.setText(initialText);
+	    setDescription(descText);
+	    setLabelText(label);
+	  }
+
+	  private void init() {
+
+	    this.setLayout(new GridLayout(3,1));
+
+	    descLabel = WidgetFactory.makeHTMLLabel(EMPTY_STRING, 1);
+	    this.add(descLabel);
+
+	    ////
+	    JPanel fileNamePanel = WidgetFactory.makePanel();
+	    fileNamePanel.setLayout(new BorderLayout());
+	    fileNamePanel.setMaximumSize(WizardSettings.WIZARD_CONTENT_SINGLE_LINE_DIMS);
+
+	    fileNameLabel = WidgetFactory.makeLabel(EMPTY_STRING, true);
+
+	    fileNameLabel.setBackground(Color.lightGray);
+	    fileNamePanel.add(fileNameLabel, BorderLayout.WEST);
+
+	    fileNameField = WidgetFactory.makeOneLineTextField();
+	    fileNameField.setEnabled(false);
+	    fileNameField.setEditable(false);
+	    fileNameField.setDisabledTextColor(
+	                                      WizardSettings.WIZARD_CONTENT_TEXT_COLOR);
+	    fileNamePanel.add(fileNameField, BorderLayout.CENTER);
+
+	    fileNameButton = WidgetFactory.makeJButton("locate...",
+
+	            new ActionListener() {
+
+	              public void actionPerformed(ActionEvent e) {
+
+	                final JFileChooser fc = new JFileChooser();
+	                fc.setDialogTitle("Select a data file to import...");
+	                String userdir = System.getProperty("user.dir");
+	                String homedir = System.getProperty("user.home");
+	                String osname = System.getProperty("os.name");
+	                if (lastDataDir==null) {
+	                  fc.setCurrentDirectory(new File(userdir));
+	                  if (osname.indexOf("Window")>-1) {  // a windows os
+	                    File mydocs = new File(homedir+File.separator+"My Documents");
+	                    if (mydocs.exists()) {
+	                      fc.setCurrentDirectory(mydocs);
+	                    }
+	                  }
+	                } else {  // use previous dataDirectory
+	                  fc.setCurrentDirectory(lastDataDir);
+	                }
+	                int returnVal = fc.showOpenDialog(WizardContainerFrame.frame);
+	                File file = null;
+	                if (returnVal == JFileChooser.APPROVE_OPTION) {
+
+	                  file = fc.getSelectedFile();
+
+	                  if (file!=null) {
+	                    lastDataDir = new File(file.getParent());
+	                    setImportFileURL(file.getAbsolutePath());
+	                    fileNameField.setText(getImportFileURL());
+	                  }
+	                }
+	              }
+	            });
+
+	    fileNamePanel.add(fileNameButton, BorderLayout.EAST);
+
+	    this.add(fileNamePanel);
+	  }
+
+	  public JLabel  getLabel()  { return this.fileNameLabel; }
+
+	  public void setLabelText(String text) { this.fileNameLabel.setText(text); }
+
+	  public JButton getButton() { return this.fileNameButton; }
+
+	  public JTextField getTextArea() { return this.fileNameField; }
+
+	  public String  getImportFileURL() { return this.importFileURL; }
+
+	  public void setDescription(String desc) {
+
+	    if (desc==null) desc = EMPTY_STRING;
+	    descLabel.setText(desc);
+	  }
+
+	  public void setImportFileURL(String filePath) {
+
+	    this.importFileURL = filePath;
+	  }
+	  
+	  /**
+	   * Sets the chosen file name
+	   * @param fileName
+	   */
+	  public void setChosenFileName(String fileName)
+	  {
+		  if(fileName != null)
+		  {
+			  fileNameField.setText(fileName);
+			  setImportFileURL(fileName);
+		  }
+	  }
+	}
