be2ec47367c22a0d7b4ca18d703af3ae509074ca
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2010-06-01 19:54:46 +0000

    catch missing resource errors and use the lookup key as the returned string (better than null?).  the exception could prevent Morpho from launching and having blanks could be confusing. errors are logged when these exceptions are thrown.

--- src/edu/ucsb/nceas/morpho/Language.java
+++ src/edu/ucsb/nceas/morpho/Language.java
@@ -3,6 +3,8 @@ package edu.ucsb.nceas.morpho;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import edu.ucsb.nceas.utilities.Log;
+
 public class Language {
 	
 	private ResourceBundle lan = null;
@@ -10,11 +12,15 @@ public class Language {
 	private static Language instance = null;
 	
 	private Language() {
-		//Locale locale = new Locale("en", "US");
-		//Locale locale = new Locale("zh", "TW");
-		//Locale locale = new Locale("zh", "CN");
-		//lan = ResourceBundle.getBundle("language.Messages", locale);
-		lan = ResourceBundle.getBundle("language.Messages");
+		try {
+			//Locale locale = new Locale("en", "US");
+			//Locale locale = new Locale("zh", "TW");
+			//Locale locale = new Locale("zh", "CN");
+			//lan = ResourceBundle.getBundle("language.Messages", locale);
+			lan = ResourceBundle.getBundle("language.Messages");
+		} catch (Exception e) {
+			Log.debug(5, "Could not load language resource bundle. " + e.getMessage());
+		}
 	}
 	
 	public static Language getInstance() {
@@ -24,9 +30,16 @@ public class Language {
 		return instance;
 	}
 	
-	public String getMessages(String  lanId) {
-		String messages = lan.getString(lanId);
-		return messages;
+	public String getMessages(String  key) {
+		String message = null;
+		try {
+			message = lan.getString(key);
+		} catch (Exception e) {
+			// probably missing the key
+			Log.debug(20, "Could not load string for language key: " + key);
+			message = key;
+		}
+		return message;
 	}
 
 }
