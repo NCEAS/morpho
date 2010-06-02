5b50eb2ed50abf0de38df0a882bc8fdae55b3dd9
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2010-06-02 17:44:16 +0000

    allow other plugins (semtools) to add their own language resource bundles - typically when the plugin is initialized.

--- src/edu/ucsb/nceas/morpho/Language.java
+++ src/edu/ucsb/nceas/morpho/Language.java
@@ -1,23 +1,27 @@
 package edu.ucsb.nceas.morpho;
 
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import edu.ucsb.nceas.utilities.Log;
 
 public class Language {
 	
-	private ResourceBundle lan = null;
+	public static String DEFAULT_LANGUAGE_BUNDLE = "language.Messages";
+
+	private List<ResourceBundle> languageBundles = null;
 	
 	private static Language instance = null;
 	
+	/**
+	 * Initialize the default language bundle - core of Morpho
+	 */
 	private Language() {
 		try {
-			//Locale locale = new Locale("en", "US");
-			//Locale locale = new Locale("zh", "TW");
-			//Locale locale = new Locale("zh", "CN");
-			//lan = ResourceBundle.getBundle("language.Messages", locale);
-			lan = ResourceBundle.getBundle("language.Messages");
+			languageBundles = new ArrayList<ResourceBundle>();
+			ResourceBundle rb = ResourceBundle.getBundle(DEFAULT_LANGUAGE_BUNDLE);
+			languageBundles.add(rb);
 		} catch (Exception e) {
 			Log.debug(5, "Could not load language resource bundle. " + e.getMessage());
 		}
@@ -30,16 +34,39 @@ public class Language {
 		return instance;
 	}
 	
+	/**
+	 * Looks up the message for the given key
+	 * Searches all registered resource bundles, returning the first key found
+	 * If no entry is found, the key is returned back
+	 * @param key string to look up
+	 * @return the message for given key
+	 */
 	public String getMessage(String  key) {
 		String message = null;
-		try {
-			message = lan.getString(key);
-		} catch (Exception e) {
-			// probably missing the key
-			Log.debug(20, "Could not load string for language key: " + key);
-			message = key;
+		// loop through all registered bundles
+		for (ResourceBundle bundle: languageBundles) {
+			try {
+				message = bundle.getString(key);
+			} catch (Exception e) {
+				// probably missing the key, will continue searching
+				Log.debug(20, "Could not load string for language key: " + key);
+			}
+			if (message != null) {
+				return message;
+			}
 		}
-		return message;
+		
+		// return the key if we got this far
+		return key;
 	}
-
+	
+	/**
+	 * Register another language bundle with the system
+	 * Plugins can then inject their own bundles without modifying the base Morpho code
+	 * @param baseName name of the properties resource to add (load)
+	 */
+	public void addLanguageBundle(String baseName) {
+		ResourceBundle rb = ResourceBundle.getBundle(baseName);
+		languageBundles.add(rb);
+	}
 }
