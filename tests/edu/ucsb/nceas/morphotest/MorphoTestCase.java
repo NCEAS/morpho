314e98e0fc32771180fb1c5a2e5667a105c943bc
Jing Tao <tao@nceas.ucsb.edu>
2012-09-05 03:57:01 +0000

    Add a base test case morpho. This case can get the current configuration file and profile.

---
+++ tests/edu/ucsb/nceas/morphotest/MorphoTestCase.java
@@ -0,0 +1,64 @@
+package edu.ucsb.nceas.morphotest;
+
+import java.io.File;
+import java.io.IOException;
+
+
+import edu.ucsb.nceas.morpho.Morpho;
+import edu.ucsb.nceas.morpho.framework.ConfigXML;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+
+/**
+ * Based test case for morpho
+ * @author tao
+ *
+ */
+public class MorphoTestCase extends TestCase
+{
+  
+  protected static Morpho morpho;
+  protected static ConfigXML config = null;
+  protected static ConfigXML profile;
+  
+  static {
+    try {
+      File configDir = new File(ConfigXML.getConfigDirectory());
+      File configFile = new File(configDir, "config.xml");
+      config = new ConfigXML(configFile.getAbsolutePath());
+      File currentProfileLocation = new File(configDir, "currentprofile.xml");
+      ConfigXML currentProfileConfig = new ConfigXML(currentProfileLocation.getAbsolutePath());
+      String currentProfileName = currentProfileConfig.get("current_profile", 0);
+      String profileDirName = config.getConfigDirectory()+
+      File.separator+config.get("profile_directory", 0)+
+      File.separator+currentProfileName;
+      //System.out.println("the profile dir is "+profileDirName);
+      File profileLocation = new File(profileDirName, currentProfileName+".xml");
+      profile = new ConfigXML(profileLocation.getAbsolutePath());
+  } catch (IOException ioe) {
+    fail("Test failed, couldn't create config."+ioe.getMessage());
+  }
+  }
+  /**
+   * Constructor to build the test
+   */
+  public MorphoTestCase() 
+  {
+      super();
+  }
+
+  /**
+   * Constructor to build the test
+   *
+   * @param name the name of the test method
+   */
+  public MorphoTestCase(String name) 
+  {
+      super(name);
+  }
+  
+ 
+
+}
--- tests/edu/ucsb/nceas/morphotest/datastore/idmanagement/IdManagerTest.java
+++ tests/edu/ucsb/nceas/morphotest/datastore/idmanagement/IdManagerTest.java
@@ -32,36 +32,15 @@ import java.io.IOException;
 import edu.ucsb.nceas.morpho.Morpho;
 import edu.ucsb.nceas.morpho.datastore.idmanagement.IdentifierManager;
 import edu.ucsb.nceas.morpho.framework.ConfigXML;
+import edu.ucsb.nceas.morphotest.MorphoTestCase;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestResult;
 import junit.framework.TestSuite;
-public class IdManagerTest extends TestCase
+public class IdManagerTest extends MorphoTestCase
 {
 
-  private static Morpho morpho;
-  private static ConfigXML config = null;
-  private static String docid="tao.12104.1";
-  private static ConfigXML profile = null;
-  static {
-        try {
-            File configDir = new File(ConfigXML.getConfigDirectory());
-            File configFile = new File(configDir, "config.xml");
-            config = new ConfigXML(configFile.getAbsolutePath());
-            File currentProfileLocation = new File(configDir, "currentprofile.xml");
-            ConfigXML currentProfileConfig = new ConfigXML(currentProfileLocation.getAbsolutePath());
-            String currentProfileName = currentProfileConfig.get("current_profile", 0);
-            String profileDirName = config.getConfigDirectory()+
-        File.separator+config.get("profile_directory", 0)+
-        File.separator+currentProfileName;
-            //System.out.println("the profile dir is "+profileDirName);
-            File profileLocation = new File(profileDirName, currentProfileName+".xml");
-            profile = new ConfigXML(profileLocation.getAbsolutePath());
-          
-        } catch (IOException ioe) {
-          fail("Test failed, couldn't create config."+ioe.getMessage());
-        }
-  }
+  
   
   /**
    * Constructor to build the test
