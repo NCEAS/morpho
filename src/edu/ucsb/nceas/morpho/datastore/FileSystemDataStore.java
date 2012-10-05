e438f859de13fb5d5662f5a2445342720251431b
Jing Tao <tao@nceas.ucsb.edu>
2012-10-05 23:10:14 +0000

    Add the new class for FileSystemDataStore and its junit test.

---
+++ src/edu/ucsb/nceas/morpho/datastore/FileSystemDataStore.java
@@ -0,0 +1,200 @@
+/**
+ *  '$RCSfile: DataStoreInterface.java,v $'
+ *  Copyright: 2000 Regents of the University of California and the
+ *              National Center for Ecological Analysis and Synthesis
+ *    Authors: @authors@
+ *    Release: @release@
+ *
+ *   '$Author: tao $'
+ *     '$Date: 2008-12-19 23:58:56 $'
+ * '$Revision: 1.5 $'
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package edu.ucsb.nceas.morpho.datastore;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import java.util.UUID;
+
+import edu.ucsb.nceas.morpho.datastore.idmanagement.IdentifierFileMap;
+import edu.ucsb.nceas.morpho.exception.IdentifierNotFoundException;
+
+
+/**
+ * A class represents a data store. The Data store is specified as a file path string.
+ * It can provide and save data.
+ * @author tao
+ *
+ */
+public class FileSystemDataStore {
+  private static final String MORPHO = "Morpho-";
+  private String directory = null;
+  private IdentifierFileMap idFileMap = null;
+  private File storeDirectory = null;
+  
+  
+  /**
+   * Constructor.
+   * @param directory - the file path of the data store.
+   * @throws FileNotFoundException
+   * @throws IOException
+   * @throws UnsupportedCharsetException
+   * @throws IllegalCharsetNameException
+   * @throws NullPointerException
+   * @throws IllegalArgumentException
+   */
+  public FileSystemDataStore(String directory) throws FileNotFoundException, 
+                    IOException, UnsupportedCharsetException, IllegalCharsetNameException, 
+                    NullPointerException, IllegalArgumentException {
+   init(directory);
+  }
+  
+  /*
+   * Initialize the IdentifierFileMap object
+   */
+  private void init(String directory) throws FileNotFoundException, 
+  IOException, UnsupportedCharsetException, IllegalCharsetNameException, 
+  NullPointerException, IllegalArgumentException {
+    if(directory == null || directory.trim().equals("")) {
+      throw new NullPointerException("FileSystemDataStore.init - the specified file path for the store shouldn't be null or blank.");
+    }
+    this.directory = directory;
+    this.storeDirectory = new File(this.directory);
+    if(!this.storeDirectory.exists()) {
+      boolean success = this.storeDirectory.mkdirs();
+      if(!success) {
+        throw new IOException("FileSystemDataStore.init - morpho can't create the directory at the location "+storeDirectory.getAbsolutePath());
+      }
+    }
+    if(!this.storeDirectory.isDirectory()) {
+      throw new IllegalArgumentException("FileSystemDataStore.init - the specified file path for the store should be a directory. "+
+                                          "However, "+storeDirectory.getAbsolutePath()+" is not a directory.");
+      
+    }
+    idFileMap = new IdentifierFileMap(this.storeDirectory);
+  }
+  
+  
+  /**
+   * Get the OutputStream object for a specified identifier.
+   * @param identifier - the specified identifier.
+   * @return the source of the data associated with the identifier
+   * @throws IdentifierNotFoundException
+   * @throws FileNotFoundException
+   */
+  public InputStream get(String identifier) 
+                  throws IdentifierNotFoundException, FileNotFoundException {
+    return new FileInputStream(idFileMap.getFile(identifier));
+  }
+  
+  
+  /**
+   * Store an InputStream object (data) into the store with the specified identifier.
+   * @param identifier - the identifier of the data object
+   * @param data - the source to get the data
+   * @throws FileNotFoundException
+   * @throws IOException
+   */
+  public void set(String identifier, InputStream data) throws FileNotFoundException, IOException, NullPointerException{
+    if(identifier == null ) {
+      throw new NullPointerException("FileSystemDataStrore.set - the specified identifier shouldn't be null.");
+    }
+    if(data == null ) {
+      throw new NullPointerException("FileSystemDataStrore.set - the InputStream object of the data shouldn't be null.");
+    }
+    
+    BufferedInputStream buffData = null;
+    FileOutputStream out = null;
+    BufferedOutputStream buffOut = null;
+    try {
+    //String fileName = generateFileName(identifier);
+      buffData = new BufferedInputStream(data);
+      File file = generateFile(identifier);
+      out = new FileOutputStream(file);
+      buffOut = new BufferedOutputStream(out);
+       
+      int d = -1;
+      while((d =buffData.read()) != -1) {
+        buffOut.write(d); //write out everything in the reader    
+      }
+      buffOut.flush();
+      idFileMap.setMap(identifier, file);
+    } finally {
+      if(out != null) {
+        out.close();
+      }
+      if(buffOut != null) {
+        buffOut.close();
+      }
+      if(data != null ) {
+        data.close();
+      }
+      if(buffData != null) {
+        buffData.close();
+      }
+           
+    }
+    
+   
+    
+    
+  }
+  
+  /*
+   * Generate a unique file name for the identifier.
+   */
+  private File generateFile(String identifier) throws IOException {
+    
+    //String fileName = UUID.randomUUID().toString();
+    //String fileName = (new Integer(identifier.hashCode())).toString();
+    File file = File.createTempFile(MORPHO, "", storeDirectory);
+    return file;
+  }
+  
+  /**
+   * Get the file path of the data store.
+   * @return the file path as a String object
+   */
+  public String getDirectory() {
+    return this.directory;
+  }
+  
+  /**
+   * Change the file path of the data store
+   * @param directory - the file path of the data store
+   * @throws FileNotFoundException
+   * @throws IOException
+   * @throws UnsupportedCharsetException
+   * @throws IllegalCharsetNameException
+   * @throws NullPointerException
+   * @throws IllegalArgumentException
+   */
+  public void setDirectory(String directory) throws FileNotFoundException, 
+                     IOException, UnsupportedCharsetException, IllegalCharsetNameException, 
+                     NullPointerException, IllegalArgumentException {
+    init(directory);
+  }
+  
+}
---
+++ tests/edu/ucsb/nceas/morphotest/datastore/FileSystemDataStoreTest.java
@@ -0,0 +1,144 @@
+/**
+ *  '$RCSfile: EML201DocumentCorrectorTest.java,v $'
+ *  Copyright: 2000 Regents of the University of California and the
+ *              National Center for Ecological Analysis and Synthesis
+ *    Authors: @authors@
+ *    Release: @release@
+ *
+ *   '$Author: tao $'
+ *     '$Date: 2008-09-25 22:56:13 $'
+ * '$Revision: 1.3 $'
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package edu.ucsb.nceas.morphotest.datastore;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import edu.ucsb.nceas.morpho.datastore.FileSystemDataStore;
+import edu.ucsb.nceas.morpho.exception.IdentifierNotFoundException;
+import edu.ucsb.nceas.morphotest.MorphoTestCase;
+
+
+/**
+ * A junit test for the FileSystemDataStore class.
+ * @author tao
+ *
+ */
+public class FileSystemDataStoreTest extends MorphoTestCase {
+  
+  private static final String objectStorePath1 = "build/tests";
+  private static final String objectStorePath2 = "build/tmp";
+  private static final String id1 = "tao.111";
+  private static final String id2 = "tao.222";
+  private static final String id3 = "jing.123";
+  private static final String id4 = "jing.321";
+  private static final String filePath1 = "build/tests/testfiles/eml201-reference-system.xml";
+  private static final String filePath2 = "build/tests/testfiles/eml201withadditionalMetacat.xml";
+  /**
+   * Create a suite of tests to be run together
+   */
+  public static Test suite() throws Exception {
+      TestSuite suite = new TestSuite();
+      suite.addTest(new FileSystemDataStoreTest("initialize"));
+      suite.addTest(new FileSystemDataStoreTest("testSet"));
+      suite.addTest(new FileSystemDataStoreTest("testGet"));
+      return suite;
+  }
+  
+  /**
+   * Constructor a test
+   * @param name the name of test
+   */
+  public FileSystemDataStoreTest(String name) {
+    super(name);
+  }
+  
+  /**
+   * Check that the testing framework is functioning properly with 
+   * a trivial assertion.
+   */
+  public void initialize() {
+      assertTrue(true);
+  }
+  
+  /**
+   * Test set methods (including setDirectory and getDirectory methods)
+   * @throws Exception
+   */
+  public void testSet() throws Exception {
+    FileSystemDataStore fileStore = new FileSystemDataStore(objectStorePath1);
+    fileStore.set(id1, new FileInputStream(new File(filePath1)));
+    fileStore.set(id2, new FileInputStream(new File(filePath2)));
+    assertTrue("FileSystemDataStore.testSet - the directory should be "+objectStorePath1+ 
+        " rather than "+fileStore.getDirectory(), objectStorePath1.equals(fileStore.getDirectory()));
+    fileStore.setDirectory(objectStorePath2);
+    fileStore.set(id3, new FileInputStream(new File(filePath1)));
+    fileStore.set(id4, new FileInputStream(new File(filePath2)));
+    assertTrue("FileSystemDataStore.testSet - the directory should be "+objectStorePath2+ 
+        " rather than "+fileStore.getDirectory(), objectStorePath2.equals(fileStore.getDirectory()));
+  }
+  
+  /**
+   * Test get method
+   * @throws Exception
+   */
+  public void testGet() throws Exception {
+    FileSystemDataStore fileStore = new FileSystemDataStore(objectStorePath1);
+    InputStream in = fileStore.get(id1);
+    File f1 = File.createTempFile("test", null);
+    FileOutputStream output = new FileOutputStream(f1);
+    byte[] array = new byte[8*1024];
+    int index = -1;
+    while ((index = in.read(array)) != -1) {
+      output.write(array, 0, index);
+    }
+    in.close();
+    output.close();
+    assertTrue("The file "+f1.getAbsolutePath()+" should have the same size of file "+filePath1, 
+        f1.length() == (new File(filePath1).length()));
+    fileStore.setDirectory(objectStorePath2);
+    boolean inException = false;
+    try {
+      InputStream input = fileStore.get(id2);
+    } catch(IdentifierNotFoundException e) {
+      inException = true;
+    }
+    assertTrue("FileSystemDataStore.testGet - we shouldn't find the id "+id2+
+        " in the store since the store location has been changed.", inException);
+    
+    InputStream in2 = fileStore.get(id4);
+    File f2 = File.createTempFile("test", null);
+    FileOutputStream output2 = new FileOutputStream(f2);
+    byte[] array2 = new byte[8*1024];
+    int index2 = -1;
+    while ((index2 = in2.read(array2)) != -1) {
+      output2.write(array2, 0, index2);
+    }
+    in2.close();
+    output2.close();
+    assertTrue("The file "+f2.getAbsolutePath()+" should have the same size of file "+filePath2, 
+        f2.length() == (new File(filePath2).length()));
+    
+  }
+  
+
+}
