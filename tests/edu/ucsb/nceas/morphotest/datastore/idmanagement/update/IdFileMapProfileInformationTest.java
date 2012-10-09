e322674bd2a1006309245c864c4d08b59fddcdfc
Jing Tao <tao@nceas.ucsb.edu>
2012-10-09 18:41:11 +0000

    A junit test class.

---
+++ tests/edu/ucsb/nceas/morphotest/datastore/idmanagement/update/IdFileMapProfileInformationTest.java
@@ -0,0 +1,54 @@
+package edu.ucsb.nceas.morphotest.datastore.idmanagement.update;
+
+import java.io.File;
+import java.util.Vector;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import edu.ucsb.nceas.morpho.datastore.idmanagement.update.IdFileMapProfileInformation;
+import edu.ucsb.nceas.morphotest.MorphoTestCase;
+
+
+public class IdFileMapProfileInformationTest extends MorphoTestCase {
+  /**
+   * Constructor to build the test
+   *
+   * @param name the name of the test method
+   */
+  public IdFileMapProfileInformationTest(String name) {
+      super(name);
+     
+  }
+  /**
+   * Create a suite of tests to be run together
+   */
+  public static Test suite() {
+      TestSuite suite = new TestSuite();
+      suite.addTest(new IdFileMapProfileInformationTest("initialize"));
+      suite.addTest(new IdFileMapProfileInformationTest("testGetStatusAndDirectories"));
+      
+      return suite;
+  }
+  
+  /**
+   * Check that the testing framework is functioning properly with 
+   * a trivial assertion.
+   */
+  public void initialize()
+  {
+      assertTrue(true);
+  }
+  
+  /**
+   * Test the getUpdateStatus and the getObjectDirectories methods
+   */
+  public void testGetStatusAndDirectories() {
+    IdFileMapProfileInformation info = new IdFileMapProfileInformation(profile);
+    System.out.println("The profile being added the id-file mapping is "+info.getUpdatedStatus());
+    Vector<File> list = info.getObjectDirectories();
+    for(File file : list) {
+      System.out.println("The directories needs to be updated are "+file.getAbsolutePath());
+    }
+  }
+
+}
