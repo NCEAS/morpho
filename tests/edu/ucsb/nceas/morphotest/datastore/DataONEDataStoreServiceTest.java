78cb386dd188be7ddd1b1aa64738557962b77e9e
Jing Tao <tao@nceas.ucsb.edu>
2012-11-06 23:21:59 +0000

    Add test save and generate id methods.

--- tests/edu/ucsb/nceas/morphotest/datastore/DataONEDataStoreServiceTest.java
+++ tests/edu/ucsb/nceas/morphotest/datastore/DataONEDataStoreServiceTest.java
@@ -24,10 +24,44 @@
  */
 package edu.ucsb.nceas.morphotest.datastore;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
+import org.dataone.client.D1Object;
+import org.dataone.client.MNode;
+import org.dataone.client.ObjectFormatCache;
+import org.dataone.client.auth.CertificateManager;
+import org.dataone.client.auth.ClientIdentityManager;
+import org.dataone.ore.ResourceMapFactory;
+import org.dataone.service.exceptions.BaseException;
+import org.dataone.service.types.v1.Checksum;
+import org.dataone.service.types.v1.Identifier;
+import org.dataone.service.types.v1.NodeReference;
+import org.dataone.service.types.v1.ObjectFormat;
+import org.dataone.service.types.v1.ObjectFormatIdentifier;
+import org.dataone.service.types.v1.Subject;
+import org.dataone.service.types.v1.SystemMetadata;
+import org.dataone.service.types.v1.util.ChecksumUtil;
+
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import edu.ucsb.nceas.morpho.Morpho;
+import edu.ucsb.nceas.morpho.datapackage.AbstractDataPackage;
+import edu.ucsb.nceas.morpho.datapackage.DataPackageFactory;
+import edu.ucsb.nceas.morpho.datapackage.MorphoDataPackage;
 import edu.ucsb.nceas.morpho.datastore.DataONEDataStoreService;
+import edu.ucsb.nceas.morpho.datastore.idmanagement.IdentifierFileMap;
 import edu.ucsb.nceas.morphotest.MorphoTestCase;
 
 /**
@@ -36,6 +70,16 @@ import edu.ucsb.nceas.morphotest.MorphoTestCase;
  *
  */
 public class DataONEDataStoreServiceTest extends MorphoTestCase {
+  
+  private static final String OREPATH = "tests/testfiles/ore-package/ore";
+  private static final String EMLPATH = "tests/testfiles/ore-package/eml.xml";
+  private static final String DATAPATH = "tests/testfiles/ore-package/data.txt";
+  private static final String OREFORMATID = "http://www.openarchives.org/ore/terms";
+  private static final String EMLFORMATID = "eml://ecoinformatics.org/eml-2.0.0";
+  private static final String DATAFORMATID = "text/csv";
+  private static final String DATAID = "data-identifier";
+  private DataONEDataStoreService service = null;
+  private Subject submitter = null;
   /**
    * Create a suite of tests to be run together
    */
@@ -43,6 +87,7 @@ public class DataONEDataStoreServiceTest extends MorphoTestCase {
       TestSuite suite = new TestSuite();
       suite.addTest(new DataONEDataStoreServiceTest("initialize"));
       suite.addTest(new DataONEDataStoreServiceTest("testExists"));
+      suite.addTest(new DataONEDataStoreServiceTest("testSave"));
       return suite;
   }
   
@@ -52,6 +97,7 @@ public class DataONEDataStoreServiceTest extends MorphoTestCase {
    */
   public DataONEDataStoreServiceTest(String name) {
     super(name);
+    service = new DataONEDataStoreService(Morpho.thisStaticInstance);
   }
   
   /**
@@ -63,13 +109,162 @@ public class DataONEDataStoreServiceTest extends MorphoTestCase {
   }
   
  
+  /**
+   * Test the exists method
+   * @throws Exception
+   */
   public void testExists() throws Exception {
     String id = "ornl.mstmip.benchmark.global.gpp.modis.01";
     String id2= "tao.1";
-    DataONEDataStoreService service = new DataONEDataStoreService(Morpho.thisStaticInstance);
+    
     assertTrue("the id "+id+" should exist in the server ", service.exists(id));
     assertFalse("The id "+id2+" shouldn't exist in the server", service.exists(id2));
     
+  }
+  
+  
+  /**
+   * Test the save method
+   * @throws Exception
+   */
+  public void testSave() throws Exception {
+    CertificateManager.getInstance().setCertificateLocation(getCertificateFileLocation());
+    submitter = ClientIdentityManager.getCurrentIdentity();
+    MorphoDataPackage dataPackage = createMorphoPackage();
+    service.save(dataPackage);
+  }
+  
+  /*
+   * Create a morpho data package.
+   */
+  private MorphoDataPackage createMorphoPackage() throws Exception {
+    String oreIdStr = service.generateIdentifier("tao");
+    System.out.println("the oreIdStr is "+oreIdStr);
+    String metadataIdStr = service.generateIdentifier("tao");
+    System.out.println(" the metadataIdStr is "+metadataIdStr);
+    String dataIdStr = service.generateIdentifier("tao");
+    System.out.println(" the dataIdStr is "+dataIdStr);
+    
+    MNode activeNode = service.getActiveMNode();
+    NodeReference node = new NodeReference();
+    node.setValue(activeNode.getNodeId());
+    
+    Identifier oreId = new Identifier();
+    oreId.setValue(oreIdStr);
+    MorphoDataPackage ore = new MorphoDataPackage();
+    ore.setPackageId(oreId);
+    Map<Identifier, List<Identifier>> metadataRelation = new HashMap();
+    Identifier metadataId = new Identifier();
+    metadataId.setValue(metadataIdStr);
+    List<Identifier> list = new ArrayList<Identifier>();
+    Identifier dataId = new Identifier();
+    dataId.setValue(dataIdStr);
+    metadataRelation.put(metadataId, list);
+    ore.setMetadataMap(metadataRelation);
+    ObjectFormatIdentifier oreFormatId = new ObjectFormatIdentifier();
+    oreFormatId.setValue(OREFORMATID);
+    SystemMetadata oreSysMeta = generateSystemMetadata(oreId, ore.serializePackage().getBytes(), 
+                                                      oreFormatId, submitter,node); 
+    ore.setSystemMetadata(oreSysMeta);
+    
+    String eml = IOUtils.toString(new FileInputStream(new File(EMLPATH)));
+    eml.replaceAll(DATAID, dataIdStr);
+    AbstractDataPackage adp = DataPackageFactory.getDataPackage(new StringReader(eml));
+    byte[] emlByte = eml.getBytes();
+    adp.setData(emlByte);
+    ObjectFormatIdentifier emlFormatId = new ObjectFormatIdentifier();
+    emlFormatId.setValue(EMLFORMATID);
+    SystemMetadata emlSysMeta =  generateSystemMetadata(metadataId, emlByte, 
+        emlFormatId, submitter,node); 
+    adp.setSystemMetadata(emlSysMeta);
+    ore.addData(adp);
+    
+    String data = IOUtils.toString(new FileInputStream(new File(DATAPATH)));
+    D1Object dataObject = new D1Object();
+    dataObject.setData(data.getBytes());
+    ObjectFormatIdentifier dataFormatId = new ObjectFormatIdentifier();
+    dataFormatId.setValue(DATAFORMATID);
+    SystemMetadata dataSysMeta =  generateSystemMetadata(dataId, data.getBytes(), 
+       dataFormatId, submitter,node); 
+    dataObject.setSystemMetadata(dataSysMeta);
+    ore.addData(dataObject);
+    return ore;
+    
+   /*InputStream ore = new FileInputStream(new File(OREPATH));
+    String oreContent = IOUtils.toString(ore, IdentifierFileMap.UTF8);
+    //DataPackage dataPackage = DataPackage.deserializePackage(oreContent);
+    Map<Identifier, Map<Identifier, List<Identifier>>> packageMap = 
+        ResourceMapFactory.getInstance().parseResourceMap(oreContent);
+    Identifier pid = packageMap.keySet().iterator().next();
+    System.out.println("the package id is "+pid.getValue());
+    Map <Identifier, List<Identifier>> metadataMap = packageMap.get(pid);
+    Identifier metadataId = metadataMap.keySet().iterator().next();
+    System.out.println("the metadata id is "+metadataId.getValue());
+    List<Identifier> list = metadataMap.get(metadataId);
+    for (Identifier id : list) {
+      System.out.println("the data ids are "+id.getValue());
+    }*/
+    
+    
+  }
+  
+  /*
+   * Generate the system metadata.
+   */
+  private SystemMetadata generateSystemMetadata(Identifier id, byte[] data, 
+      ObjectFormatIdentifier formatId, Subject submitter, NodeReference nodeId) 
+          throws Exception{
+
+    SystemMetadata sm = new SystemMetadata();
+    sm.setIdentifier(id);
+    ObjectFormat fmt;
+    try {
+      fmt = ObjectFormatCache.getInstance().getFormat(formatId);
+    }
+    catch (BaseException be) {
+      formatId.setValue("application/octet-stream");
+      fmt = ObjectFormatCache.getInstance().getFormat(formatId);
+    }
+    sm.setFormatId(fmt.getFormatId());
+
+    //create the checksum
+    InputStream is = new ByteArrayInputStream(data);
+
+    Checksum checksum;
+    checksum = ChecksumUtil.checksum(is, "MD5");
+    sm.setChecksum(checksum);
+
+    //set the size
+    sm.setSize(new BigInteger(String.valueOf(data.length)));
+
+    // serializer needs a value, though MN will ignore the value
+    sm.setSerialVersion(BigInteger.ONE);
+    
+    // set submitter and rightholder from the associated string
+    sm.setSubmitter(submitter);
+    sm.setRightsHolder(submitter);
+    
+    Date dateCreated = new Date();
+    sm.setDateUploaded(dateCreated);
+    Date dateUpdated = new Date();
+    sm.setDateSysMetadataModified(dateUpdated);
+
+    // Node information
+    sm.setOriginMemberNode(nodeId);
+    sm.setAuthoritativeMemberNode(nodeId);
+
+    return sm;
+  }
+
+  /*
+   * The certificate always locates on the temperate folder and it has a fixed name/tmp/x509up_u1000 
+   * 
+   */
+  private String getCertificateFileLocation()
+  {
+    String tmpDir = System.getProperty("java.io.tmpdir");
+    File file =new File(tmpDir,"x509up_u1000");
+    return file.getAbsolutePath();
   }
 }
 
