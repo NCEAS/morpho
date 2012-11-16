479254bb429570c2ddd1057a5f596957a4cc4e84
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2012-11-16 17:48:29 +0000

    save ADP in the DataPackage list of objects. preping to remove the adp member variable from this MDP class

--- src/edu/ucsb/nceas/morpho/datapackage/MorphoDataPackage.java
+++ src/edu/ucsb/nceas/morpho/datapackage/MorphoDataPackage.java
@@ -27,7 +27,11 @@
 
 package edu.ucsb.nceas.morpho.datapackage;
 
+import java.util.Iterator;
+
+import org.dataone.client.D1Object;
 import org.dataone.client.DataPackage;
+import org.dataone.service.types.v1.Identifier;
 
 /**
  * <p>A class that represents a data package in Morpho. This extends from DataONE's
@@ -36,16 +40,34 @@ import org.dataone.client.DataPackage;
  */
 public class MorphoDataPackage extends DataPackage {
 	
-	// TODO: need to save in the D1Object collection, not just as member variable
+ 
+	/**
+	 * TODO: remove this field
+	 * @deprecated need to save in the D1Object collection, not just as member variable
+	 */
 	private AbstractDataPackage adp = null;
 	
 	public AbstractDataPackage getAbstractDataPackage() {
-		// TODO: look up the ADP from the DataPackage D1Object collection
+		// look up the ADP from the DataPackage D1Object collection?
+		AbstractDataPackage tempAdp = null;
+		Iterator<Identifier> metadataIdentifiers = this.getMetadataMap().keySet().iterator();
+		while (metadataIdentifiers.hasNext()) {
+			Identifier id = metadataIdentifiers.next();
+			D1Object d1Object = this.get(id);
+			if (d1Object instanceof AbstractDataPackage) {
+				tempAdp = (AbstractDataPackage) d1Object;
+				return tempAdp;
+			}
+		}
+		
+		// TODO: don't keep reference here
 		return adp;
 	}
 	
 	public void setAbstractDataPackage(AbstractDataPackage adp) {
-		// TODO: save in the DataPackage collection
+		// save in the DataPackage collection
+		this.addData(adp);
+		// TODO: don't keep reference here
 		this.adp = adp;
 	}
 
