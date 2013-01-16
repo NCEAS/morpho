b85dc06a4f5060746f291e7a174ed139b530aa76
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2013-01-16 22:32:16 +0000

    Merge from 1.10.0 branch: strip CSV quotes from column values when presenting code definition and taxonomic import wizard screens. The escape quotes are not desired in this context and a use would otherwise have to manually remove them from each imported value. http://bugzilla.ecoinformatics.org/show_bug.cgi?id=5783

--- src/edu/ucsb/nceas/morpho/plugins/datapackagewizard/pages/CodeDefnPanel.java
+++ src/edu/ucsb/nceas/morpho/plugins/datapackagewizard/pages/CodeDefnPanel.java
@@ -946,6 +946,7 @@ public class CodeDefnPanel extends JPanel implements WizardPageSubPanelAPI {
           int cnt = -1;
           while( st.hasMoreTokens() ) {
             token = st.nextToken().trim();
+            token = stripCSVQuotes(token);
             cnt++;
             int idx = -1;
             if((idx = colIndices.indexOf(new Integer(cnt))) >  -1) {
@@ -968,6 +969,7 @@ public class CodeDefnPanel extends JPanel implements WizardPageSubPanelAPI {
           StringTokenizer st = new StringTokenizer(line, delimiter, true);
           while( st.hasMoreTokens() ) {
             token = st.nextToken().trim();
+            token = stripCSVQuotes(token);
             if (! (delimiter.indexOf(token) > -1) ) {
               cnt++;
               int idx = -1;
@@ -1029,6 +1031,20 @@ public class CodeDefnPanel extends JPanel implements WizardPageSubPanelAPI {
   }// end of function getcolumnValues
 
 
+
+  /**
+   * Removes matched single or double quotes surrounding the given string.
+   * @param token
+   * @return the token with quotes removed if they existed
+   * @see http://bugzilla.ecoinformatics.org/show_bug.cgi?id=5783
+   */
+  private static String stripCSVQuotes(String token) {
+	if ( (token.startsWith("\"") && token.endsWith("\"")) 
+			|| (token.startsWith("'") && token.endsWith("'"))) {
+		token = token.substring(1, token.length()-1);
+	}
+	return token;
+  }
 
   public static List getOneColumnValue(File file, int colIndex, int numHeaderLines, String delimiter, int maxLinesNeeded) {
 
